ecf95f37b726dfeca93b9e65b608bdd3f4bb8bc9
johannes hanika <hanatos@gmail.com>
2019-06-05 19:50:31 +1200

    gui: some comments/docs

--- src/gui/render.h
+++ src/gui/render.h
@@ -1,8 +1,22 @@
+#pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// initialise imgui. has to go after sdl and vulkan surface have been inited.
+// will steal whatever is needed from global qvk struct.
 int dt_gui_init_imgui();
 
-void dt_gui_poll_event(SDL_Event *event)
+// tear down imgui and free resources
+void dt_gui_cleanup_imgui();
 
+// poll imgui events from given event
+void dt_gui_poll_event(SDL_Event *event);
+
+// render imgui frame
 void dt_gui_render_frame();
 
-void dt_gui_cleanup_imgui();
+#ifdef __cplusplus
+}
+#endif
