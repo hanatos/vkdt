da8a4619e15bb61019648f1858e8f32f97d0b087
johannes hanika <hanatos@gmail.com>
2019-06-07 04:55:08 +1200

    build: gui compiles again

--- src/gui/flat.mk
+++ src/gui/flat.mk
@@ -1,9 +1,16 @@
+# TODO: remove demo if not needed:
 GUI_O=gui/gui.o\
       gui/render.o\
-      gui/main.o
+      gui/main.o\
+      ../ext/imgui/imgui.o\
+      ../ext/imgui/imgui_draw.o\
+      ../ext/imgui/imgui_widgets.o\
+      ../ext/imgui/imgui_demo.o\
+      ../ext/imgui/examples/imgui_impl_vulkan.o\
+      ../ext/imgui/examples/imgui_impl_sdl.o
 GUI_H=gui/gui.h\
       gui/render.h\
       gui/imgui_impl_sdl.h\
       gui/imgui_impl_vulkan.h
-GUI_CFLAGS=$(shell pkg-config --cflags sdl2) -I../ext/imgui
-GUI_LDFLAGS=-ldl $(shell pkg-config --libs sdl2)
+GUI_CFLAGS=$(shell pkg-config --cflags sdl2) -I../ext/imgui -I../ext/imgui/examples/
+GUI_LDFLAGS=-ldl $(shell pkg-config --libs sdl2) -lm -lstdc++
