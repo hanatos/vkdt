4a190b512a4974542c302d6e5a288748741f54d5
johannes hanika <hanatos@gmail.com>
2019-06-14 20:57:04 +1200

    build: add vulkan sdk install instructions to run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -2,5 +2,6 @@
 VULKAN_SDK=/opt/vulkan/1.1.106.0/x86_64
 export PATH=${PATH}:${VULKAN_SDK}/bin
 export LD_LIBRARY_PATH=${VULKAN_SDK}/lib
-# and the layers are in ~/.local/share/vulkan/explicit_layer.d.
+# and the layers are in ~/.local/share/vulkan/explicit_layer.d, i.e.:
+# mkdir -p ~/.local/share/vulkan && cp -r ${VULKAN_SDK}/etc/explicit_layer.d/ ~/.local/share/vulkan
 ./vkdt -g gui.cfg -d all
