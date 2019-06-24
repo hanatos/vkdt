f102d4cd883d5d46ace41d151b04b440ca0b40eb
johannes hanika <hanatos@gmail.com>
2019-06-24 10:46:15 +0200

    ..now with shaders, too

---
+++ src/pipe/modules/contrast/blur_head.glsl
@@ -0,0 +1,19 @@
+#include "shared.glsl"
+
+layout(local_size_x = 32, local_size_y = 32, local_size_z = 1) in;
+
+// global uniform stuff about image and roi
+layout(std140, set = 0, binding = 0) uniform params_t
+{
+  roi_t ri;
+  roi_t ro;
+} params;
+
+
+layout( // input f32 buffer rgb
+    set = 1, binding = 0
+) uniform sampler2D img_in;
+
+layout( // output f32 buffer rgb
+    set = 1, binding = 1, rgba32f
+) uniform image2D img_out;
