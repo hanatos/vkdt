86ea6d7ee11451d712483d907aba69f03e235a68
johannes hanika <hanatos@gmail.com>
2019-07-12 00:34:09 +1200

    cli: add options to dump graphs to stdout

--- src/cli/main.c
+++ src/cli/main.c
@@ -1,6 +1,7 @@
 #include "qvk/qvk.h"
 #include "pipe/graph.h"
 #include "pipe/graph-io.h"
+#include "pipe/graph-print.h"
 #include "pipe/global.h"
 #include "core/log.h"
 
@@ -14,14 +15,19 @@ int main(int argc, char *argv[])
   dt_pipe_global_init();
 
   const char *graphcfg = 0;
+  int dump_graph = 0;
   for(int i=0;i<argc;i++)
   {
     if(!strcmp(argv[i], "-g") && i < argc-1)
       graphcfg = argv[++i];
+    else if(!strcmp(argv[i], "--dump-modules"))
+      dump_graph = 1;
+    else if(!strcmp(argv[i], "--dump-nodes"))
+      dump_graph = 2;
   }
   if(!graphcfg)
   {
-    dt_log(s_log_cli, "usage: vkdt-cli -g <graph.cfg> [-d verbosity]");
+    dt_log(s_log_cli, "usage: vkdt-cli -g <graph.cfg> [-d verbosity] [--dump-modules|--dump-nodes]");
     exit(1);
   }
   if(qvk_init()) exit(1);
@@ -36,6 +42,11 @@ int main(int argc, char *argv[])
   }
 
   dt_graph_run(&graph, s_graph_run_all);
+
+  if(dump_graph == 1)
+    dt_graph_print_modules(&graph);
+  else if(dump_graph == 2)
+    dt_graph_print_nodes(&graph);
 
   dt_graph_cleanup(&graph);
   qvk_cleanup();
