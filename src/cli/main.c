0e24a7784fce45d92d43b72444b94440e95081d5
johannes hanika <hanatos@gmail.com>
2019-09-03 09:41:58 +1200

    cli: replace floating point srgb by 8-bit if needed

--- src/cli/main.c
+++ src/cli/main.c
@@ -20,9 +20,9 @@ replace_display(
   if(mid < 0) return 1; // no display node by that name
 
   // get module the display is connected to:
-  const int cid = dt_module_get_connector(graph->module+mid, dt_token_static("input"));
-  const int m0 = graph->module[mid].connector[cid].connected_mi;
-  const int c0 = graph->module[mid].connector[cid].connected_mc;
+  int cid = dt_module_get_connector(graph->module+mid, dt_token_static("input"));
+  int m0 = graph->module[mid].connector[cid].connected_mi;
+  int c0 = graph->module[mid].connector[cid].connected_mc;
 
   if(m0 < 0) return 2; // display input not connected
 
@@ -30,11 +30,17 @@ replace_display(
   // maybe new module 8-bit in between here
   if(ldr)
   {
-    const int m1 = dt_module_add(graph, dt_token_static("f2srgb"),  inst);
+    const int m1 = dt_module_add(graph, dt_token_static("f2srgb8"),  inst);
     const int c1 = dt_module_get_connector(graph->module+m1, dt_token_static("input"));
     const int m2 = dt_module_add(graph, dt_token_static("export8"), inst);
     const int c2 = dt_module_get_connector(graph->module+m2, dt_token_static("input"));
 
+    if(graph->module[m0].name == dt_token("f2srgb"))
+    { // detect and skip f2srgb (replaced by f2srgb8)
+      cid = dt_module_get_connector(graph->module+m0, dt_token_static("input"));
+      c0 = graph->module[m0].connector[cid].connected_mc;
+      m0 = graph->module[m0].connector[cid].connected_mi;
+    }
     // connect: source (m0, c0) -> destination (m1, c1)
     dt_module_connect(graph, m0, c0, m1, c1);
     dt_module_connect(graph, m1, c1, m2, c2);
@@ -72,6 +78,9 @@ int main(int argc, char *argv[])
 
   const char *graphcfg = 0;
   int dump_graph = 0;
+  dt_token_t output = dt_token("main");
+  const char *filename = "output";
+  int ldr = 1;
   for(int i=0;i<argc;i++)
   {
     if(!strcmp(argv[i], "-g") && i < argc-1)
@@ -80,6 +89,7 @@ int main(int argc, char *argv[])
       dump_graph = 1;
     else if(!strcmp(argv[i], "--dump-nodes"))
       dump_graph = 2;
+    // TODO: parse more output: filename, format related things etc
   }
   if(!graphcfg)
   {
@@ -96,6 +106,16 @@ int main(int argc, char *argv[])
     dt_log(s_log_err, "could not load graph configuration from '%s'!", graphcfg);
     exit(1);
   }
+
+  // replace requested display node by export node:
+  err = replace_display(&graph, output, ldr, filename);
+  if(err)
+  {
+    dt_log(s_log_err, "graph does not contain suitable display node %"PRItkn"!", dt_token_str(output));
+    exit(2);
+  }
+  // make sure all remaining display nodes are removed:
+  disconnect_display_nodes(&graph);
 
   dt_graph_run(&graph, s_graph_run_all);
 
