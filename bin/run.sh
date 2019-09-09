66171fe3c0a9f79567cddc174acdd3bf50dbb712
johannes hanika <hanatos@gmail.com>
2019-09-09 14:17:07 +0200

    run.sh: fix PATH variable

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VK_SDK_PATH=/opt/vulkan/1.1.114.0/x86_64
-export PATH=${PATH}:${VK_SDK_PATH}/bin
+export PATH=${VK_SDK_PATH}/bin:${PATH}
 export LD_LIBRARY_PATH=${VK_SDK_PATH}/lib
 # and the layers are in ~/.local/share/vulkan/explicit_layer.d, i.e.:
 # mkdir -p ~/.local/share/vulkan && cp -r ${VK_SDK_PATH}/etc/vulkan/explicit_layer.d/ ~/.local/share/vulkan
