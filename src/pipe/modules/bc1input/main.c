e8d060af7a96b1427ce74462d40050bcf7a821fb
johannes hanika <hanatos@gmail.com>
2019-09-22 21:42:49 +1200

    pipe: gzipped bc1

--- src/pipe/modules/bc1input/main.c
+++ src/pipe/modules/bc1input/main.c
@@ -2,27 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-int init(dt_module_t *mod)
-{
-  // jpginput_buf_t *dat = malloc(sizeof(*dat));
-  // memset(dat, 0, sizeof(*dat));
-  // mod->data = dat;
-  return 0;
-}
-
-void cleanup(dt_module_t *mod)
-{
-  if(!mod->data) return;
-  // jpginput_buf_t *jpg = mod->data;
-  // if(jpg->filename[0])
-  // {
-  //   if(jpg->f) fclose(jpg->f);
-  //   jpg->filename[0] = 0;
-  // }
-  // free(jpg);
-  // mod->data = 0;
-}
+#include <zlib.h>
 
 // this callback is responsible to set the full_{wd,ht} dimensions on the
 // regions of interest on all "write"|"source" channels
@@ -30,21 +10,50 @@ void modify_roi_out(
     dt_graph_t  *graph,
     dt_module_t *mod)
 {
-  // load image if not happened yet
-  // const char *filename = dt_module_param_string(mod, 0);
-  // if(read_header(mod, filename)) return;
-  // jpginput_buf_t *jpg = mod->data;
-  // mod->connector[0].roi.full_wd = jpg->width;
-  // mod->connector[0].roi.full_ht = jpg->height;
+  // load only header
+  const char *filename = dt_module_param_string(mod, 0);
+
+  uint32_t header[4] = {0};
+  gzFile f = gzopen(filename, "rb");
+  if(!f || gzread(f, header, sizeof(uint32_t)*4) != sizeof(uint32_t)*4)
+  {
+    fprintf(stderr, "[bc1input] %s: can't open file!\n", filename);
+    if(f) gzclose(f);
+  }
+  // checks: magic != dt_token("bc1z") || version != 1
+  if(header[0] != dt_token("bc1z") || header[1] != 1)
+  {
+    fprintf(stderr, "[bc1input] %s: wrong magic number or version!\n", filename);
+    gzclose(f);
+  }
+  const uint32_t wd = header[2], ht = header[3];
+  mod->connector[0].roi.full_wd = wd;
+  mod->connector[0].roi.full_ht = ht;
 }
 
 int read_source(
     dt_module_t *mod,
     void *mapped)
 {
-  // const char *filename = dt_module_param_string(mod, 0);
-  // if(read_header(mod, filename)) return 1;
-  // jpginput_buf_t *jpg = mod->data;
-  // jpeg_read(jpg, mapped);
+  const char *filename = dt_module_param_string(mod, 0);
+
+  uint32_t header[4] = {0};
+  gzFile f = gzopen(filename, "rb");
+  if(!f || gzread(f, header, sizeof(uint32_t)*4) != sizeof(uint32_t)*4)
+  {
+    fprintf(stderr, "[bc1input] %s: can't open file!\n", filename);
+    if(f) gzclose(f);
+    return 1;
+  }
+  // checks: magic != dt_token("bc1z") || version != 1
+  if(header[0] != dt_token("bc1z") || header[1] != 1)
+  {
+    fprintf(stderr, "[bc1input] %s: wrong magic number or version!\n", filename);
+    gzclose(f);
+    return 1;
+  }
+  const int wd = header[2], ht = header[3];
+  gzread(f, mapped, sizeof(uint8_t)*8*(wd/4)*(ht/4));
+  gzclose(f);
   return 0;
 }
--- src/pipe/modules/bc1out/main.cc
+++ src/pipe/modules/bc1out/main.cc
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <squish.h>
+#include <zlib.h>
 
 // called after pipeline finished up to here.
 // our input buffer will come in memory mapped.
@@ -15,8 +16,8 @@ extern "C" void write_sink(
   const char *basename = dt_module_param_string(module, 0);
   fprintf(stderr, "[bc1out] writing '%s'\n", basename);
 
-  const int wd = module->connector[0].roi.wd;
-  const int ht = module->connector[0].roi.ht;
+  const uint32_t wd = module->connector[0].roi.wd;
+  const uint32_t ht = module->connector[0].roi.ht;
   const uint8_t *in = (const uint8_t *)buf;
 
   char filename[512];
@@ -24,10 +25,12 @@ extern "C" void write_sink(
 
   // go through all 4x4 blocks and call squish::CompressMasked
   // TODO: parallelise via our thread pool or openmp or what?
-  uint8_t *out = (uint8_t *)malloc(sizeof(uint8_t)*8*wd*ht);
-  for(int j=0;j<ht;j+=4)
+  const int bx = wd/4, by = ht/4;
+  size_t num_blocks = bx * by;
+  uint8_t *out = (uint8_t *)malloc(sizeof(uint8_t)*8*num_blocks);
+  for(int j=0;j<4*by;j+=4)
   {
-    for(int i=0;i<wd;i+=4)
+    for(int i=0;i<4*bx;i+=4)
     {
       // swizzle block data together:
       uint8_t block[64];
@@ -35,13 +38,15 @@ extern "C" void write_sink(
         for(int ii=0;ii<4;ii++)
           for(int c=0;c<4;c++)
             block[4*(4*jj+ii)+c] = in[4*(wd*(j+jj)+(i+ii))+c];
-      squish::CompressMasked(block, 0xffff, out + 8*(wd/4*j/4+i/4),
+      squish::CompressMasked(block, 0xffff, out + 8*(bx*j/4+i/4),
           squish::kDxt1 | squish::kColourRangeFit);
     }
   }
 
-  FILE* f = fopen(filename, "wb");
-  // TODO: write magic, version, width, height
-  fwrite(out, sizeof(uint8_t)*8, wd*ht, f);
-  fclose(f);
+  gzFile f = gzopen(filename, "wb");
+  // write magic, version, width, height
+  uint32_t header[4] = { dt_token("bc1z"), 1, wd, ht };
+  gzwrite(f, header, sizeof(uint32_t)*4);
+  gzwrite(f, out, sizeof(uint8_t)*8*num_blocks);
+  gzclose(f);
 }
