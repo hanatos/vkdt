82a902aa18d0dc9db2bb02b99cf59173ef8c19fd
johannes hanika <hanatos@gmail.com>
2019-09-29 03:52:00 +1300

    exposure: don't destroy shader-generated images

--- src/pipe/modules/exposure/main.c
+++ src/pipe/modules/exposure/main.c
@@ -7,10 +7,17 @@ void commit_params(dt_graph_t *graph, dt_node_t *node)
   float *f = (float *)node->module->committed_param;
   for(int k=0;k<4;k++)
     f[k] = node->module->img_param.black[k]/65535.0f;
-  for(int k=0;k<4;k++)
-    f[4+k] = powf(2.0f, ((float*)node->module->param)[0]) *
-       node->module->img_param.whitebalance[k] * 65535.0f /
-      (node->module->img_param.white[k]-node->module->img_param.black[k]);
+  if(node->module->img_param.whitebalance[0] > 0 &&
+     node->module->img_param.white[0] > 0)
+  {
+    for(int k=0;k<4;k++)
+      f[4+k] = powf(2.0f, ((float*)node->module->param)[0]) *
+         node->module->img_param.whitebalance[k] * 65535.0f /
+        (node->module->img_param.white[k]-node->module->img_param.black[k]);
+  }
+  else
+    for(int k=0;k<4;k++)
+      f[4+k] = powf(2.0f, ((float*)node->module->param)[0]);
 }
 
 int init(dt_module_t *mod)
