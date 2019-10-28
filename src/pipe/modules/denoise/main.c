2ec464ef56d00533e0f87dfe5f07d5fdee68b825
johannes hanika <hanatos@gmail.com>
2019-10-28 21:39:48 +1300

    pipe: add dummy "denoise" module

---
+++ src/pipe/modules/denoise/main.c
@@ -0,0 +1,41 @@
+#include "modules/api.h"
+
+void modify_roi_in(
+    dt_graph_t *graph,
+    dt_module_t *module)
+{
+  // request the full thing, we want the borders
+  module->connector[0].roi.wd = module->connector[0].roi.full_wd;
+  module->connector[0].roi.ht = module->connector[0].roi.full_ht;
+  module->connector[0].roi.x = 0.0f;
+  module->connector[0].roi.y = 0.0f;
+}
+
+void modify_roi_out(
+    dt_graph_t *graph,
+    dt_module_t *module)
+{
+  // copy to output
+  const uint32_t *b = module->img_param.crop_aabb;
+  module->connector[1].roi = module->connector[0].roi;
+  module->connector[1].roi.full_wd = b[2] - b[0];
+  module->connector[1].roi.full_ht = b[3] - b[1];
+}
+
+void commit_params(dt_graph_t *graph, dt_node_t *node)
+{
+  uint32_t *i = (uint32_t *)node->module->committed_param;
+  const uint32_t *b = node->module->img_param.crop_aabb;
+  for(int k=0;k<4;k++) i[k] = b[k];
+  float *f = (float *)node->module->committed_param;
+  for(int k=0;k<4;k++)
+    f[k+4] = node->module->img_param.black[k]/65535.0f;
+  for(int k=0;k<4;k++)
+    f[k+8] = node->module->img_param.white[k]/65535.0f;
+}
+
+int init(dt_module_t *mod)
+{
+  mod->committed_param_size = sizeof(uint32_t)*12;
+  return 0;
+}
