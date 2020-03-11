8112e0cb97c8611117a61a6c05cffc0f0e8fda1a
johannes hanika <hanatos@gmail.com>
2020-03-11 13:14:25 +0100

    build: remove obsolute run.sh
    at least debian has new enough vulkan validation layers now

--- bin/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-VK_SDK_PATH=/opt/vulkan/1.1.130.0/x86_64
-export PATH=${VK_SDK_PATH}/bin:${PATH}
-export LD_LIBRARY_PATH=${VK_SDK_PATH}/lib
-# and the layers are in ~/.local/share/vulkan/explicit_layer.d, i.e.:
-# mkdir -p ~/.local/share/vulkan && cp -r ${VK_SDK_PATH}/etc/vulkan/explicit_layer.d/ ~/.local/share/vulkan
-gdb --args ./vkdt -g gui.cfg -d all
