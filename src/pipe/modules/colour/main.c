6d733ddf3826627bc7c8d4440a9551949789e29f
johannes hanika <hanatos@gmail.com>
2020-08-07 19:48:39 +0200

    colour: use identity matrix if not initialised

--- src/pipe/modules/colour/main.c
+++ src/pipe/modules/colour/main.c
@@ -299,13 +299,14 @@ void commit_params(dt_graph_t *graph, dt_module_t *module)
       f[k] = powf(2.0f, ((float*)module->param)[0]);
 
   // grab params by name:
-  const float *p_wb  = dt_module_param_float(module, dt_module_get_param(module->so, dt_token("wb")));
+  // const float *p_wb  = dt_module_param_float(module, dt_module_get_param(module->so, dt_token("wb")));
   const int    p_cnt = dt_module_param_int  (module, dt_module_get_param(module->so, dt_token("cnt")))[0];
   const float *p_src = dt_module_param_float(module, dt_module_get_param(module->so, dt_token("source")));
   const float *p_tgt = dt_module_param_float(module, dt_module_get_param(module->so, dt_token("target")));
-  const int    p_mat = dt_module_param_int  (module, dt_module_get_param(module->so, dt_token("matrix")))[0];
+        int    p_mat = dt_module_param_int  (module, dt_module_get_param(module->so, dt_token("matrix")))[0];
   const int    p_gam = dt_module_param_int  (module, dt_module_get_param(module->so, dt_token("gamut")))[0];
   const int    p_mod = dt_module_param_int  (module, dt_module_get_param(module->so, dt_token("mode")))[0];
+  if(!(module->img_param.cam_to_rec2020[0] > 0)) p_mat = 0; // no matrix? default to identity
   if(p_mat == 1)
   { // the one that comes with the image from the source node:
     for(int j=0;j<3;j++) for(int i=0;i<3;i++)
