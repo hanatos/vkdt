1116d171385273fa7ed0220765cb5c5300ff7db5
johannes hanika <hanatos@gmail.com>
2020-08-30 22:53:32 +0200

    denoise: use noise profile

--- src/pipe/modules/dev/main.c
+++ src/pipe/modules/dev/main.c
@@ -3,6 +3,30 @@
 #include <stdlib.h>
 
 void
+commit_params(
+    dt_graph_t  *graph,
+    dt_module_t *module)
+{
+  float *f = (float *)module->committed_param;
+  float *g = (float*)module->param;
+  f[0] = g[0];
+  f[1] = g[1];
+  f[2] = g[2];
+  f[3] = 0.0f;// module->img_param.black[1]; // XXX TODO: set this to zero after removing black level!
+  // TODO: how to do this? we can't overwrite img_param in denoise since we need it next time
+  // TODO: and we can't access the (potentially multiple) modules connected to our output
+  f[4] = module->img_param.noise_a;
+  f[5] = module->img_param.noise_b;
+}
+
+int
+init(dt_module_t *mod)
+{
+  mod->committed_param_size = sizeof(float)*6;
+  return 0;
+}
+
+void
 create_nodes(
     dt_graph_t  *graph,
     dt_module_t *module)
@@ -11,8 +35,7 @@ create_nodes(
   const int ht = module->connector[0].roi.ht;
   const int dp = 1;
 
-  // XXX TODO: wire 5 scales of downsample + assembly node
-
+  // wire 4 scales of downsample + assembly node
   int id_down[4] = {0};
 
   for(int i=0;i<4;i++)
@@ -21,28 +44,28 @@ create_nodes(
     id_down[i] = graph->num_nodes++;
     graph->node[id_down[i]] = (dt_node_t) {
       .name   = dt_token("dev"),
-        .kernel = dt_token("down"),
-        .module = module,
-        .wd     = wd,
-        .ht     = ht,
-        .dp     = dp,
-        .num_connectors = 2,
-        .connector = {{
-          .name   = dt_token("input"),
-          .type   = dt_token("read"),
-          .chan   = dt_token("rgba"),
-          .format = dt_token("f16"),
-          .roi    = module->connector[0].roi,
-          .connected_mi = -1,
-        },{
-          .name   = dt_token("output"),
-          .type   = dt_token("write"),
-          .chan   = dt_token("rgba"),
-          .format = dt_token("f16"),
-          .roi    = module->connector[0].roi,
-        }},
-        .push_constant_size = sizeof(uint32_t),
-        .push_constant = { i },
+      .kernel = dt_token("down"),
+      .module = module,
+      .wd     = wd,
+      .ht     = ht,
+      .dp     = dp,
+      .num_connectors = 2,
+      .connector = {{
+        .name   = dt_token("input"),
+        .type   = dt_token("read"),
+        .chan   = dt_token("rgba"),
+        .format = dt_token("f16"),
+        .roi    = module->connector[0].roi,
+        .connected_mi = -1,
+      },{
+        .name   = dt_token("output"),
+        .type   = dt_token("write"),
+        .chan   = dt_token("rgba"),
+        .format = dt_token("f16"),
+        .roi    = module->connector[0].roi,
+      }},
+      .push_constant_size = sizeof(uint32_t),
+      .push_constant = { i },
     };
   }
   // wire inputs:
