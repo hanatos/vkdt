e1fd39458b86a291526932164ca3fc1eea4642e2
johannes hanika <hanatos@gmail.com>
2020-09-02 16:25:16 +0200

    draw: update example cfg and make it work again

--- src/pipe/modules/blend/main.c
+++ src/pipe/modules/blend/main.c
@@ -6,7 +6,7 @@ void commit_params(dt_graph_t *graph, dt_module_t *module)
   float *f = (float *)module->committed_param;
   uint32_t *i = (uint32_t *)module->committed_param;
   float *g = (float*)module->param;
-  i[0] = module->img_param.filters;
+  i[0] = module->connector[0].chan == dt_token("rggb") ? module->img_param.filters : (graph->frame_cnt > 1 ? 0u : 1u);
   f[1] = g[0];
   f[2] = module->img_param.black[0]/(float)0xffff;
   f[3] = module->img_param.white[0]/(float)0xffff;
