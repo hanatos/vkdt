4db81c0252af151614d05d6c67b08e38f78dcefd
johannes hanika <hanatos@gmail.com>
2020-10-20 21:01:49 +0200

    noise profile: add directory if not there

--- bin/noise-profile.sh
+++ bin/noise-profile.sh
@@ -4,6 +4,7 @@ if [ "$#" -ne 1 ]; then
     exit -1
 fi
 fname=$(./vkdt-cli -g examples/noiseprofile.cfg --config param:i-raw:01:filename:$1 | grep nprof | cut -d\' -f2)
+mkdir -p data/nprof
 mv "$fname" data/nprof/
 ./vkdt-cli -g examples/noisecheck.cfg --filename "$fname" --config param:i-raw:01:filename:$1
 
