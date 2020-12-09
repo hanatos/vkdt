de490d8bb21517acb25b403e0d20c5d1316427a9
johannes hanika <hanatos@gmail.com>
2020-12-09 12:20:23 +0100

    pick: allow rasterised spectral output

--- src/pipe/modules/pick/main.c
+++ src/pipe/modules/pick/main.c
@@ -10,8 +10,10 @@ void modify_roi_in(
   r->x = 0;
   r->y = 0;
   r->scale = 1.0f;
-  module->connector[1].roi.wd = module->connector[1].roi.full_wd;
-  module->connector[1].roi.ht = module->connector[1].roi.full_ht;
+  module->connector[3].roi.wd = module->connector[3].roi.full_wd;
+  module->connector[3].roi.ht = module->connector[3].roi.full_ht;
+  module->connector[2].roi.wd = module->connector[2].roi.full_wd;
+  module->connector[2].roi.ht = module->connector[2].roi.full_ht;
 }
 
 void modify_roi_out(
@@ -19,8 +21,11 @@ void modify_roi_out(
     dt_module_t *module)
 {
   // always request constant size for picked colours:
-  module->connector[1].roi.full_wd = 20; // max 20 areas picked with mean rgb
-  module->connector[1].roi.full_ht = 4;  // and variances etc in different lines
+  module->connector[3].roi.full_wd = 20; // max 20 areas picked with mean rgb
+  module->connector[3].roi.full_ht = 4;  // and variances etc in different lines
+  // rasterise spectra at this resolution
+  module->connector[2].roi.full_wd = 500;
+  module->connector[2].roi.full_ht = 250;
 }
 
 void
@@ -61,8 +66,8 @@ create_nodes(
     .name   = dt_token("pick"),
     .kernel = dt_token("sink"),
     .module = module,
-    .wd     = module->connector[1].roi.wd,
-    .ht     = module->connector[1].roi.ht,
+    .wd     = module->connector[3].roi.wd,
+    .ht     = module->connector[3].roi.ht,
     .dp     = 1,
     .num_connectors = 1,
     .connector = {{
@@ -70,14 +75,49 @@ create_nodes(
       .type   = dt_token("sink"),
       .chan   = dt_token("r"),
       .format = dt_token("ui32"),
+      .roi    = module->connector[3].roi,
+      .connected_mi = -1,
+    }},
+  };
+  assert(graph->num_nodes < graph->max_nodes);
+  const int id_dspy = graph->num_nodes++;
+  graph->node[id_dspy] = (dt_node_t) {
+    .name   = dt_token("pick"),
+    .kernel = dt_token("display"),
+    .module = module,
+    .wd     = module->connector[2].roi.wd,
+    .ht     = module->connector[2].roi.ht,
+    .dp     = 1,
+    .num_connectors = 3,
+    .connector = {{
+      .name   = dt_token("input"),
+      .type   = dt_token("read"),
+      .chan   = dt_token("r"),
+      .format = dt_token("ui32"),
+      .roi    = module->connector[3].roi,
+      .connected_mi = -1,
+    },{
+      .name   = dt_token("lut"),
+      .type   = dt_token("read"),
+      .chan   = dt_token("rgba"),
+      .format = dt_token("f16"),
       .roi    = module->connector[1].roi,
       .connected_mi = -1,
+    },{
+      .name   = dt_token("dspy"),
+      .type   = dt_token("write"),
+      .chan   = dt_token("rgba"),
+      .format = dt_token("f16"),
+      .roi    = module->connector[2].roi,
     }},
   };
 
   // interconnect nodes:
   dt_connector_copy(graph, module, 0, id_collect, 0);
   dt_node_connect  (graph, id_collect, 1, id_map, 0);
+  dt_node_connect  (graph, id_collect, 1, id_dspy, 0);
+  dt_connector_copy(graph, module, 1, id_dspy, 1);
+  dt_connector_copy(graph, module, 2, id_dspy, 2);
   // dt_connector_copy(graph, module, 1, id_collect, 1);
   graph->node[id_collect].connector[1].flags = s_conn_clear; // restore after connect
 }
@@ -89,8 +129,8 @@ void write_sink(
     void *buf)
 {
   uint32_t *u32 = buf;
-  const int wd = module->connector[1].roi.wd;
-  const int ht = module->connector[1].roi.ht; // we only read the first moment
+  const int wd = module->connector[3].roi.wd;
+  const int ht = module->connector[3].roi.ht; // we only read the first moment
 
   // now read back what we averaged and write into param "picked"
   float *picked = 0;
