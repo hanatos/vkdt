32501689704ff93c053d321aed5711a13953bbb4
johannes hanika <hanatos@gmail.com>
2021-01-13 11:47:41 +0100

    gamut: bilateral n- vs u-shaped texture lookup

--- src/pipe/modules/spec/flat.mk
+++ src/pipe/modules/spec/flat.mk
@@ -16,15 +16,15 @@ clean_spec_luts:
 	rm -f abney.lut spectra.lut macadam.lut
 
 macadam.lut: pipe/modules/spec/macadam
-	@echo "precomputing max theoretical reflectance brightness.."
+	@echo "[spectral lut] precomputing max theoretical reflectance brightness.."
 	pipe/modules/spec/macadam
 
 ../bin/data/spectra.lut: pipe/modules/spec/mkspectra macadam.lut
-	@echo "precomputing rgb to spectrum upsampling table.."
+	@echo "[spectral lut] precomputing rgb to spectrum upsampling table.."
 	pipe/modules/spec/mkspectra 512 /dev/null XYZ
 	mv spectra.lut ../bin/data/
 
 ../bin/data/abney.lut: pipe/modules/spec/mkabney macadam.lut
-	@echo "precomputing saturation table to compensate the abney effect.."
+	@echo "[spectral lut] precomputing saturation table to compensate the abney effect.."
 	pipe/modules/spec/mkabney 2048 /dev/null XYZ
 	mv abney.lut ../bin/data/
