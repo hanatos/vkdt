f61df13f390c5fb152a92d88b19cacb7c0ef70ab
johannes hanika <hanatos@gmail.com>
2021-02-04 13:11:37 +0100

    pipe: be more verbose if file can't be found and search searchpath also from cli

--- src/pipe/graph-export.c
+++ src/pipe/graph-export.c
@@ -76,13 +76,14 @@ dt_graph_export(
     int err = dt_graph_read_config_ascii(graph, param->p_cfgfile);
     if(err)
     {
+      dt_log(s_log_pipe, "could not open config file '%s'.", param->p_cfgfile);
       dt_token_t input_module = param->input_module;
       if(param->input_module == 0) input_module = dt_token("i-raw");
       char graph_cfg[256];
       if(param->p_defcfg)
         snprintf(graph_cfg, sizeof(graph_cfg), "%s", param->p_defcfg);
       else
-        snprintf(graph_cfg, sizeof(graph_cfg), "default-darkroom.%"PRItkn, dt_token_str(input_module));
+        snprintf(graph_cfg, sizeof(graph_cfg), "%s/default-darkroom.%"PRItkn, dt_pipe.basedir, dt_token_str(input_module));
       err = dt_graph_read_config_ascii(graph, graph_cfg);
       char imgfilename[256];
       // follow link if this is a cfg in a tag collection:
