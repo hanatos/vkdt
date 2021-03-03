57af56ec9b510c686fa29de669ba25a66f9a034f
johannes hanika <hanatos@gmail.com>
2021-03-03 08:43:08 +0100

    rt: debug tweaks

--- src/pipe/modules/burst/main.c
+++ src/pipe/modules/burst/main.c
@@ -422,7 +422,9 @@ create_nodes(
   dt_connector_copy(graph, module, 1, id_warp, 0);
   CONN(dt_node_connect(graph, id_offset, 2, id_warp, 1));
   dt_connector_copy(graph, module, 2, id_warp, 2);
-  dt_connector_copy(graph, module, 3, id_off[0], 3);  // full res mask
-  graph->node[id_off[0]].connector[3].roi = roi[1];   // XXX FIXME: connector_copy should probably respect ROI
+  dt_connector_copy(graph, module, 3, id_off[1], 3);  // lo res mask
+  graph->node[id_off[1]].connector[3].roi = roi[2];   // XXX FIXME: connector_copy should probably respect ROI
+  // dt_connector_copy(graph, module, 3, id_off[0], 3);  // full res mask
+  // graph->node[id_off[0]].connector[3].roi = roi[1];   // XXX FIXME: connector_copy should probably respect ROI
 #undef NUM_LEVELS
 }
