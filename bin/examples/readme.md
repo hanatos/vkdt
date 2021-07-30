86fa482198ab397bedcce23d0368475e3791eb4c
johannes hanika <hanatos@gmail.com>
2021-07-30 10:48:16 +0200

    examples: clean up directory make things work again

---
+++ bin/examples/readme.md
@@ -0,0 +1,10 @@
+# example projects
+
+in this directory is a loose collection of `.cfg` files with examples. to be
+able to see them, you should have some random `test.cr2`, `test.jpg` and
+`test.pfm` files as input for the graphs to work on.
+
+`vkdt` will know it's the `examples/` directory and not overwrite the files,
+so experiment away in the gui with them. your changes will be discarded at the
+end. copy the files to another directory (along with a source image file) to
+make your changes persistent.
--- bin/noise-profile.sh
+++ bin/noise-profile.sh
@@ -3,8 +3,8 @@ if [ "$#" -ne 1 ]; then
     echo "usage: noise-profile.sh <raw image file>"
     exit -1
 fi
-fname=$(./vkdt-cli -g examples/noiseprofile.cfg --config param:i-raw:01:filename:$1 | grep nprof | cut -d\' -f2)
+fname=$(./vkdt-cli -g data/noiseprofile.cfg --config param:i-raw:01:filename:$1 | grep nprof | cut -d\' -f2)
 mkdir -p data/nprof
 mv "$fname" data/nprof/
-./vkdt-cli -g examples/noisecheck.cfg --filename "$fname" --config param:i-raw:01:filename:$1
+./vkdt-cli -g data/noisecheck.cfg --filename "$fname" --config param:i-raw:01:filename:$1
 
