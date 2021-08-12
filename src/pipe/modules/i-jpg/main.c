c809f5a421606fc323e08c6bd6b11a1649a3c356
Greg V <greg@unrelenting.technology>
2021-08-12 15:34:00 +0300

    Use POSIX standard limits.h instead of linux/limits.h

--- src/pipe/modules/i-jpg/main.c
+++ src/pipe/modules/i-jpg/main.c
@@ -3,7 +3,7 @@
 #include <jpeglib.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <linux/limits.h>
+#include <limits.h>
 #include <setjmp.h>
 
 typedef struct jpginput_buf_t
--- src/pipe/modules/i-lut/main.c
+++ src/pipe/modules/i-lut/main.c
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <linux/limits.h>
+#include <limits.h>
 
 typedef struct header_t
 {
