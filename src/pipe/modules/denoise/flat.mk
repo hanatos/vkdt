f70f6408f50f4bccc076bf603050c7af177884f2
johannes hanika <hanatos@gmail.com>
2021-10-18 17:24:47 +0200

    denoise: fix deps and div 0

--- src/pipe/modules/denoise/flat.mk
+++ src/pipe/modules/denoise/flat.mk
@@ -1,6 +1,4 @@
 MOD_LDFLAGS=-lm
 MOD_C=pipe/connector.c
-pipe/modules/dn2/down.comp.spv   :pipe/modules/dn2/cov.glsl
-pipe/modules/dn2/downcov.comp.spv:pipe/modules/dn2/cov.glsl
-pipe/modules/dn2/down.comp.spv   :pipe/modules/dn2/squad.glsl
-pipe/modules/dn2/downcov.comp.spv:pipe/modules/dn2/squad.glsl
+pipe/modules/denoise/down.comp.spv   :pipe/modules/denoise/cov.glsl
+pipe/modules/denoise/downcov.comp.spv:pipe/modules/denoise/cov.glsl
