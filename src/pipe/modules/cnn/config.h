0ba1145555350d6c3553731fda2bc9b1efd93d85
johannes hanika <hanatos@gmail.com>
2021-12-04 18:24:48 +0100

    cnn: enable shmem version, at least it doesn't oom

--- src/pipe/modules/cnn/config.h
+++ src/pipe/modules/cnn/config.h
@@ -1,5 +1,6 @@
 // tweaked for performance on gtx1650 maxq (bad idea).
 // comes out at 217ms (shmem) vs 225ms (microkernels) @1080p
+// (goes down to 185 with only a border of 1.. :/ )
 #define DT_CNN_TILE_WD 24
 #define DT_CNN_TILE_HT 24
 #define DT_CNN_BORDER  2
