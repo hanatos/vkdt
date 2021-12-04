dc455dddec4a88fe81aab77b6d38cbdc19d5f361
johannes hanika <hanatos@gmail.com>
2021-12-04 12:00:52 +0100

    cnn: wire shared memory version (dysfunctional)

--- src/pipe/modules/cnn/flat.mk
+++ src/pipe/modules/cnn/flat.mk
@@ -1,2 +1,4 @@
 MOD_LDFLAGS=-lm
 MOD_C=pipe/connector.c
+pipe/modules/cnn/cnn.comp.spv   :pipe/modules/cnn/config.h
+pipe/modules/cnn/libcnn.so      :pipe/modules/cnn/config.h
--- src/pipe/modules/cnn/main.c
+++ src/pipe/modules/cnn/main.c
@@ -1,3 +1,5 @@
+#include "config.h"
+#include "modules/localsize.h"
 #include "modules/api.h"
 #include "connector.h"
 
@@ -6,6 +8,49 @@ create_nodes(
     dt_graph_t  *graph,
     dt_module_t *module)
 {
+#if 1 // shared memory
+  assert(graph->num_nodes < graph->max_nodes);
+  const uint32_t id_cnn = graph->num_nodes++;
+  const uint32_t tile_size_x = DT_CNN_TILE_WD - 2*DT_CNN_BORDER;
+  const uint32_t tile_size_y = DT_CNN_TILE_HT - 2*DT_CNN_BORDER;
+  const uint32_t num_tiles_x = (module->connector[0].roi.wd + tile_size_x - 1)/tile_size_x;
+  const uint32_t num_tiles_y = (module->connector[0].roi.ht + tile_size_y - 1)/tile_size_y;
+  const uint32_t wd = num_tiles_x * DT_LOCAL_SIZE_X;
+  const uint32_t ht = num_tiles_y * DT_LOCAL_SIZE_Y;
+  graph->node[id_cnn] = (dt_node_t) {
+    .name   = dt_token("cnn"),
+    .kernel = dt_token("cnn"),
+    .module = module,
+    .wd     = wd,
+    .ht     = ht,
+    .dp     = 4,
+    .num_connectors = 3,
+    .connector = {{
+      .name   = dt_token("input"),
+      .type   = dt_token("read"),
+      .chan   = dt_token("rgba"),
+      .format = dt_token("f16"),
+      .roi    = module->connector[0].roi,
+      .connected_mi = -1,
+    },{
+      .name   = dt_token("wgt"),
+      .type   = dt_token("read"),
+      .chan   = dt_token("rgba"),
+      .format = dt_token("f16"),
+      .roi    = module->connector[1].roi,
+      .connected_mi = -1,
+    },{
+      .name   = dt_token("output"),
+      .type   = dt_token("write"),
+      .chan   = dt_token("rgba"),
+      .format = dt_token("f16"),
+      .roi    = module->connector[0].roi,
+    }},
+  };
+  dt_connector_copy(graph, module, 0, id_cnn, 0);
+  dt_connector_copy(graph, module, 1, id_cnn, 1);
+  dt_connector_copy(graph, module, 2, id_cnn, 2);
+#else // microkernels
   // network configuration:
   // 5x5 blur appended to input
   // sum input back to result of network
@@ -140,5 +185,6 @@ create_nodes(
   dt_connector_copy(graph, module,  2, id_sum, 2);
   // dt_connector_copy(graph, module, 2, id_blur, 2); // XXX DEBUG
   // dt_node_connect(graph, id_blur, 2, id_sum, 1); // XXX DEBUG
+#endif
 }
 
