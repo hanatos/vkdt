1d374b98c4d1f53bd5fc02d923bf84ee6eefecdf
johannes hanika <hanatos@gmail.com>
2021-12-04 18:05:04 +0100

    cnn: tweak tile size for perf

--- src/pipe/modules/cnn/config.h
+++ src/pipe/modules/cnn/config.h
@@ -1,3 +1,5 @@
-#define DT_CNN_TILE_WD 32
-#define DT_CNN_TILE_HT 32
+// tweaked for performance on gtx1650 maxq (bad idea).
+// comes out at 217ms (shmem) vs 225ms (microkernels) @1080p
+#define DT_CNN_TILE_WD 24
+#define DT_CNN_TILE_HT 24
 #define DT_CNN_BORDER  2
