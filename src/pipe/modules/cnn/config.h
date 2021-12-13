8e3177438d161b2e49d058480052cd72975e3986
johannes hanika <hanatos@gmail.com>
2021-12-13 10:15:35 +0100

    cnn: tweak tile size for 2080Ti

--- src/pipe/modules/cnn/config.h
+++ src/pipe/modules/cnn/config.h
@@ -1,6 +1,9 @@
 // tweaked for performance on gtx1650 maxq (bad idea).
 // comes out at 217ms (shmem) vs 225ms (microkernels) @1080p
 // (goes down to 185 with only a border of 1.. :/ )
-#define DT_CNN_TILE_WD 24
-#define DT_CNN_TILE_HT 24
+// 2080Ti: 20 56ms, 32 200ms :(
+// 24 1border 50ms
+// 20 1border 44ms
+#define DT_CNN_TILE_WD 20
+#define DT_CNN_TILE_HT 20
 #define DT_CNN_BORDER  2
