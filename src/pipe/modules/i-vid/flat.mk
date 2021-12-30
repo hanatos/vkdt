da5d822baebf163e889be8b3cd60e2238732079d
johannes hanika <hanatos@gmail.com>
2021-12-30 13:24:44 +0100

    i-vid: silence everything in case we don't compile it in

--- src/pipe/modules/i-vid/flat.mk
+++ src/pipe/modules/i-vid/flat.mk
@@ -1,3 +1,5 @@
+ifeq ($(VKDT_USE_FFMPEG),1)
 MOD_CFLAGS=$(shell pkg-config --cflags libavformat --cflags libavcodec)
 MOD_LDFLAGS=$(shell pkg-config --libs libavformat --libs libavcodec)
 MOD_C=pipe/connector.c
+endif
