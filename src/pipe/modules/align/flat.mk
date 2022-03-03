c492e6db13510777a9c3aff677bddb4ae4862e9b
johannes hanika <hanatos@gmail.com>
2022-03-03 19:41:48 +0100

    align: some cleanups

--- src/pipe/modules/align/flat.mk
+++ src/pipe/modules/align/flat.mk
@@ -1,3 +1,4 @@
 MOD_C=pipe/connector.c
-pipe/modules/align/half.comp.spv:pipe/modules/align/config.h
-pipe/modules/align/libalign.so  :pipe/modules/align/config.h
+pipe/modules/align/half.comp.spv :pipe/modules/align/config.h
+pipe/modules/align/libalign.so   :pipe/modules/align/config.h
+pipe/modules/align/merge.comp.spv:pipe/modules/align/config.h
