f3f4d6199fa4171451ec4d9e1f895eeb2af5cb2e
johannes hanika <hanatos@gmail.com>
2022-04-25 08:51:23 +0200

    cleanup: remove/clarify old comments

--- src/pipe/params.h
+++ src/pipe/params.h
@@ -2,17 +2,14 @@
 
 #include "gui/widget_descriptor.h"
 
-// parameters to go along with every module
+// parameters to go along with every module, parsed from 'params' file
 typedef struct dt_ui_param_t
 {
-  // TODO: make it so this maps 1:1 to binary input if any.
-  // TODO: this means if this is a string argument, put it to the end
-  // TODO: such that the zero terminated string is in the right spot after this struct
   dt_token_t name;  // name of param
   dt_token_t type;  // type of param: "float" "int" "string"
   int32_t cnt;      // number of elements
   int32_t offset;   // offset into uniform buffer
-  dt_widget_descriptor_t widget; // for interoperability with gui
+  dt_widget_descriptor_t widget; // for interoperability with gui, contents of 'params.ui' go here
   union
   {
     float   val [1]; // let's assume we at least have one argument
