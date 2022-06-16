340c5f2e866077ffa23d9b16e3f374def882efea
johannes hanika <hanatos@gmail.com>
2022-06-16 12:06:37 +0200

    gui: fix deadlock when resizing the window

--- src/gui/gui.c
+++ src/gui/gui.c
@@ -361,10 +361,10 @@ VkResult dt_gui_present()
     .pImageIndices      = &vkdt.frame_index,
   };
   threads_mutex_lock(&qvk.queue_mutex);
-  QVKR(vkQueuePresentKHR(qvk.queue_graphics, &info));
+  VkResult res = vkQueuePresentKHR(qvk.queue_graphics, &info);
   threads_mutex_unlock(&qvk.queue_mutex);
   vkdt.sem_index = (vkdt.sem_index + 1) % vkdt.image_count;
-  return VK_SUCCESS;
+  return res;
 }
 
 void
