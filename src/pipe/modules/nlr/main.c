541670667fdb86ebd7c2e28078f416ac35868cf4
johannes hanika <hanatos@gmail.com>
2022-12-02 20:51:03 +0100

    nlr: compile fixes

--- src/pipe/modules/nlr/main.c
+++ src/pipe/modules/nlr/main.c
@@ -1,6 +1,5 @@
-#if 0
 #include "modules/api.h"
-#include "config.h"
+#define SVGF_IT 4
 
 void
 create_nodes(
@@ -110,51 +109,161 @@ create_nodes(
     }},
   };
 
-  int id_eaw[SVGF_IT];
+  int id_den = graph->num_nodes++;
+  graph->node[id_den] = (dt_node_t) {
+    .name   = dt_token("nlr"),
+    .kernel = dt_token("denoise"),
+    .module = module,
+    .wd     = module->connector[0].roi.wd,
+    .ht     = module->connector[0].roi.ht,
+    .dp     = 1,
+    .num_connectors = 3,
+    .connector = {{ // 0
+      .name   = dt_token("light"),
+      .type   = dt_token("read"),
+      .chan   = dt_token("rgba"),
+      .format = dt_token("*"),
+      .roi    = module->connector[0].roi,
+      .connected_mi = -1,
+    },{ // 1
+      .name   = dt_token("gbuf"),
+      .type   = dt_token("read"),
+      .chan   = dt_token("*"),
+      .format = dt_token("f32"),
+      .roi    = module->connector[0].roi,
+      .connected_mi = -1,
+    },{ // 2
+      .name   = dt_token("output"),
+      .type   = dt_token("write"),
+      .chan   = dt_token("rgba"),
+      .format = dt_token("f16"),
+      .roi    = module->connector[0].roi,
+    }},
+  };
 
+  int id_down[SVGF_IT];
+  int id_comb[SVGF_IT];
+  int id_up  [SVGF_IT];
+
+  dt_roi_t roi = module->connector[0].roi;
   for(int i=0;i<SVGF_IT;i++)
   {
-    // TODO: need a NLR downsample kernel NLR_Downsample.compute
-    // TODO: need combine kernel NLR_Combine.compute
-    // TODO: need a NLR upsample kernel NLR_DenoiseUpsample.compute
-    id_eaw[i] = graph->num_nodes++;
-    graph->node[id_eaw[i]] = (dt_node_t) {
-      .name   = dt_token("svgf"),
-      .kernel = dt_token("eaw"),
+    dt_roi_t roi_lo = roi;
+    roi_lo.wd = (roi.wd+1)/2;
+    roi_lo.ht = (roi.ht+1)/2;
+    id_down[i] = graph->num_nodes++;
+    graph->node[id_down[i]] = (dt_node_t) {
+      .name   = dt_token("nlr"),
+      .kernel = dt_token("down"),
       .module = module,
-      .wd     = module->connector[0].roi.wd,
-      .ht     = module->connector[0].roi.ht,
+      .wd     = roi_lo.wd,
+      .ht     = roi_lo.ht,
       .dp     = 1,
       .num_connectors = 4,
       .connector = {{ // 0
-        .name   = dt_token("input"),
+        .name   = dt_token("light"),
         .type   = dt_token("read"),
         .chan   = dt_token("rgba"),
         .format = dt_token("*"),
-        .roi    = module->connector[0].roi,
+        .roi    = roi,
         .connected_mi = -1,
       },{ // 1
         .name   = dt_token("gbuf"),
         .type   = dt_token("read"),
         .chan   = dt_token("*"),
         .format = dt_token("f32"),
-        .roi    = module->connector[0].roi,
+        .roi    = roi,
         .connected_mi = -1,
       },{ // 2
         .name   = dt_token("output"),
         .type   = dt_token("write"),
         .chan   = dt_token("rgba"),
         .format = dt_token("f16"),
-        .roi    = module->connector[0].roi,
+        .roi    = roi_lo,
       },{ // 3
         .name   = dt_token("gbuf_out"),
         .type   = dt_token("write"),
         .chan   = dt_token("rg"),
         .format = dt_token("f32"),
-        .roi    = module->connector[0].roi,
+        .roi    = roi_lo,
       }},
-      .push_constant_size = 1*sizeof(uint32_t),
-      .push_constant = { 1<<i },
+    };
+    id_up[i] = graph->num_nodes++;
+    graph->node[id_up[i]] = (dt_node_t) {
+      .name   = dt_token("nlr"),
+      .kernel = dt_token("up"),
+      .module = module,
+      .wd     = roi_lo.wd,
+      .ht     = roi_lo.ht,
+      .dp     = 1,
+      .num_connectors = 3,
+      .connector = {{ // 0
+        .name   = dt_token("lo"),
+        .type   = dt_token("read"),
+        .chan   = dt_token("rgba"),
+        .format = dt_token("*"),
+        .roi    = roi_lo,
+        .connected_mi = -1,
+      },{ // 1
+        .name   = dt_token("gbuf"),
+        .type   = dt_token("read"),
+        .chan   = dt_token("*"),
+        .format = dt_token("f32"),
+        .roi    = roi_lo,
+        .connected_mi = -1,
+      },{ // 2
+        .name   = dt_token("output"),
+        .type   = dt_token("write"),
+        .chan   = dt_token("rgba"),
+        .format = dt_token("f16"),
+        .roi    = roi,
+      }},
+    };
+    if(i == SVGF_IT-1) continue;
+    id_comb[i] = graph->num_nodes++;
+    graph->node[id_comb[i]] = (dt_node_t) {
+      .name   = dt_token("nlr"),
+      .kernel = dt_token("comb"),
+      .module = module,
+      .wd     = roi.wd,
+      .ht     = roi.ht,
+      .dp     = 1,
+      .num_connectors = 5,
+      .connector = {{ // 0
+        .name   = dt_token("lo"),
+        .type   = dt_token("read"),
+        .chan   = dt_token("rgba"),
+        .format = dt_token("*"),
+        .roi    = roi_lo,
+        .connected_mi = -1,
+      },{ // 1
+        .name   = dt_token("hi"),
+        .type   = dt_token("read"),
+        .chan   = dt_token("rgba"),
+        .format = dt_token("*"),
+        .roi    = roi,
+        .connected_mi = -1,
+      },{ // 2
+        .name   = dt_token("gbuf_lo"),
+        .type   = dt_token("read"),
+        .chan   = dt_token("*"),
+        .format = dt_token("f32"),
+        .roi    = roi_lo,
+        .connected_mi = -1,
+      },{ // 3
+        .name   = dt_token("gbuf_hi"),
+        .type   = dt_token("read"),
+        .chan   = dt_token("*"),
+        .format = dt_token("f32"),
+        .roi    = roi,
+        .connected_mi = -1,
+      },{ // 4
+        .name   = dt_token("output"),
+        .type   = dt_token("write"),
+        .chan   = dt_token("rgba"),
+        .format = dt_token("f16"),
+        .roi    = roi,
+      }},
     };
   }
 
@@ -164,7 +273,22 @@ create_nodes(
   dt_connector_copy(graph, module, 4, id_preblend, 3);  // previous gbuffer
   dt_connector_copy(graph, module, 5, id_preblend, 4);  // current gbuffer
 
-  CONN(dt_node_connect(graph, id_preblend, 5, id_eaw[0], 0)); // less noisy light
+  CONN(dt_node_connect(graph, id_preblend, 5, id_den, 0)); // less noisy light
+  dt_connector_copy(graph, module, 5, id_den, 1);  // current gbuffer
+
+  dt_node_connect(graph, id_den, 2, id_down[0], 0);
+  dt_connector_copy(graph, module, 5, id_down[0], 1);
+  for(int i=1;i<SVGF_IT;i++)
+  {
+    dt_node_connect(graph, id_down[i-1], 2, id_down[i], 0);
+    dt_node_connect(graph, id_down[i-1], 3, id_down[i], 1);
+
+    dt_node_connect(graph, id_down[i],   2, id_comb[i-1], 0); // col  lo
+    dt_node_connect(graph, id_down[i],   3, id_comb[i-1], 2); // gbuf lo
+    dt_node_connect(graph, id_up[i],     2, id_comb[i-1], 1); // col  hi
+    dt_node_connect(graph, id_down[i-1], 3, id_comb[i-1], 3); // gbuf hi
+  }
+#if 0
   // dt_connector_copy(graph, module, 1, id_eaw[0], 0);  // noisy light
   dt_connector_copy(graph, module, 5, id_eaw[0], 1);  // gbuffer for normal, depth, L moments
   for(int i=1;i<SVGF_IT;i++)
@@ -173,6 +297,8 @@ create_nodes(
     CONN(dt_node_connect (graph, id_eaw[i-1], 3, id_eaw[i], 1));
   }
   CONN(dt_node_connect (graph, id_eaw[SVGF_IT-1], 2, id_blend, 2)); // denoised light
+#endif
+  CONN(dt_node_connect (graph, id_comb[0], 4, id_blend, 2)); // denoised light
   CONN(dt_node_feedback(graph, id_blend, 4, id_blend, 1)); // beauty frame, old
 #if SVGF_OFF==1
   dt_connector_copy(graph, module, 1, id_blend, 2); // XXX DEBUG light w/o denoising
@@ -182,4 +308,3 @@ create_nodes(
   dt_connector_copy(graph, module, 2, id_blend, 3);  // albedo
   dt_connector_copy(graph, module, 3, id_blend, 4);  // output beauty
 }
-#endif
