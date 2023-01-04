1ae781fb9283d01b1a30a11f962841313d4e638a
johannes hanika <hanatos@gmail.com>
2023-01-04 17:21:45 +0100

    build: maybe make fedora build happy?

--- src/pipe/modules/i-mlv/flat.mk
+++ src/pipe/modules/i-mlv/flat.mk
@@ -1,3 +1,6 @@
 MOD_C=pipe/connector.c
 MOD_CFLAGS=-fopenmp
+ifeq ($(CC),clang) # https://bugzilla.redhat.com/show_bug.cgi?id=2158172
+MOD_CFLAGS+=-fno-openmp-implicit-rpath
+endif
 pipe/modules/i-mlv/libi-mlv.so: pipe/modules/i-mlv/mlv.h pipe/modules/i-mlv/raw.h pipe/modules/i-mlv/video_mlv.c pipe/modules/i-mlv/video_mlv.h
