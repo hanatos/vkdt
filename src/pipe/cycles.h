85542c6d5da7dec39fd416ee968b750be6a7d28f
johannes hanika <hanatos@gmail.com>
2023-01-14 18:49:48 +0100

    graph: traversal returns on error (instead of abort)

--- src/pipe/cycles.h
+++ src/pipe/cycles.h
@@ -64,17 +64,56 @@ dt_connection_is_cyclic(
 }
 
 static inline int
-dt_graph_is_cyclic(
+dt_graph_nodes_are_cyclic_rec(
+    dt_graph_t *g,
+    const int   nid,
+    int        *visited,
+    int        *stack,
+    const int   n0_extra)
+{
+  if(nid < 0) return 0;
+  if(!visited[nid])
+  {
+    visited[nid] = 1;
+    stack  [nid] = 1;
+    if(n0_extra >= 0)
+    { // test extra connection that is not actually on the graph yet:
+      if(!visited[n0_extra] && dt_graph_nodes_are_cyclic_rec(g, n0_extra, visited, stack, -1))
+        return 1;
+      else if(stack[n0_extra])
+        return 1;
+    }
+    for(int c=0;c<g->node[nid].num_connectors;c++)
+    {
+      dt_connector_t *cn = g->node[nid].connector+c;
+      if((cn->type == dt_token("read") || cn->type == dt_token("sink")) &&
+        !(cn->flags & s_conn_feedback))
+      { // don't visit feedback connectors, these don't constitute cycles
+        int n0 = cn->connected_mi;
+        if(n0 < 0) continue; // disconnected, may be fine
+        if(!visited[n0] && dt_graph_nodes_are_cyclic_rec(g, n0, visited, stack, -1))
+          return 1;
+        else if(stack[n0])
+          return 1;
+      }
+    }
+  }
+  stack[nid] = 0;
+  return 0;
+}
+
+static inline int
+dt_graph_nodes_are_cyclic(
     dt_graph_t *g)
 {
-  int visited[200] = {0};
-  int stack  [200] = {0};
-  assert(g->num_modules < sizeof(visited)/sizeof(visited[0]));
-  for(int m=0;m<g->num_modules;m++)
+  int visited[900] = {0};
+  int stack  [900] = {0};
+  assert(g->num_nodes < sizeof(visited)/sizeof(visited[0]));
+  for(int n=0;n<g->num_nodes;n++)
   {
-    if(!g->module[m].name) continue;
-    if(!visited[m] && dt_connection_is_cyclic_rec(g, m, visited, stack, -1))
-      return m;
+    if(!g->node[n].name) continue;
+    if(!visited[n] && dt_graph_nodes_are_cyclic_rec(g, n, visited, stack, -1))
+      return n;
   }
   return -1;
 }
