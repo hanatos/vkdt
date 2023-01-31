5cce3d3f6174b55cfafcc703afb16ce72ef762b7
johannes hanika <hanatos@gmail.com>
2023-01-31 09:41:25 +0100

    gallery: wire static web gallery creation as `vkdt gallery`

--- src/core/tools.h
+++ src/core/tools.h
@@ -12,7 +12,7 @@ dt_tool_dispatch(int argc, char *argv[])
   fs_basedir(basedir, sizeof(basedir));
   const char *cmd = 0;
   const char *cmds[] = {
-    "cli", "eval-profile", "fit", "lutinfo", "mkclut", "mkssf", // XXX noise-profile, mkgallery, read-icc ?
+    "cli", "eval-profile", "fit", "lutinfo", "mkclut", "mkssf", "gallery", // XXX noise-profile, read-icc ?
   };
   for(int i=0;i<sizeof(cmds)/sizeof(cmds[0]);i++)
     if(!strcmp(argv[1], cmds[i])) { cmd = cmds[i]; break; }
