6f05a7f6cfffb52eac9e4d1f2ed64f57ebc0c154
johannes hanika <hanatos@gmail.com>
2023-03-29 11:36:07 +0200

    quake: water params for caustics3

--- src/pipe/modules/quake/water.glsl
+++ src/pipe/modules/quake/water.glsl
@@ -2,9 +2,9 @@
 // afl_ext 2017-2019
 // https://www.shadertoy.com/view/MdXyzX
 
-#define WATER_DRAG_MULT 0.048
-#define WATER_IT 5//2 // 13
-#define WATER_IN 7//3 // 15 //48 [jo] i think 48 is pretty much emulating microfacet models directly :)
+#define WATER_DRAG_MULT 0.1 // 0.048
+#define WATER_IT 13//5//2 // 13
+#define WATER_IN 15//13//7//3 // 15 //48 [jo] i think 48 is pretty much emulating microfacet models directly :)
 
 // return heightfield of waves
 float water_height(float cl_time, vec2 position, int iterations)
