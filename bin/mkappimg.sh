104af2138948231dfb9dff20717f0842db93496b
johannes hanika <hanatos@gmail.com>
2023-10-04 10:54:29 +0200

    build: adjust library path in appimage creation

--- bin/mkappimg.sh
+++ bin/mkappimg.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+# install vkdt to AppDir
 make DESTDIR=AppDir install
 
+# create fake desktop entry + icon because apparently it's needed
 mkdir -p AppDir/usr/share/applications
 cat > AppDir/usr/share/applications/vkdt.desktop << EOF
 [Desktop Entry]
@@ -16,16 +19,20 @@ EOF
 mkdir -p AppDir/usr/share/icons/
 cp vkdt.png AppDir/usr/share/icons/
 
+cat > AppDir/AppRun << 'EOF'
+#!/bin/bash
+HERE=$(dirname $(realpath ${0}))
+export LD_LIBRARY_PATH=${HERE}/lib/x86_64-linux-gnu:${LD_LIBRARY_PATH}
+${HERE}/usr/bin/vkdt $@
+EOF
+chmod +x AppDir/AppRun
+
 if [ ! -f linuxdeploy-x86_64.AppImage ]; then
   wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 fi
 chmod +x linuxdeploy-x86_64.AppImage
-VERSION=$(git describe)
-./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage $(find AppDir/ -name "lib*.so" | sed -e 's/^/ -l /' | tr -d '\n') $(find AppDir/ -name "vkdt*" -executable | sed -e 's/^/ -e /' | tr -d '\n')
-
-if [ ! -f appimagetool-x86_64.AppImage ]; then
-  wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
-fi
-chmod +x appimagetool-x86_64.AppImage
-./appimagetool-x86_64.AppImage --sign --sign-key 2E42D54C99A903BF2F21C43CFC8D1E1D8ED4281C AppDir vkdt-${VERSION}-x86_64.AppImage 
-rm -rf AppDir/
+export VERSION=$(git describe)
+export ARCH=x86_64
+export DISABLE_COPYRIGHT_FILES_DEPLOYMENT=1
+./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage $(find AppDir/ -name "lib*.so" | sed -e 's/^/ -l /' | tr -d '\n') \
+-e AppDir/usr/bin/vkdt
