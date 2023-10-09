2aacc03d4501e6569836378ffa07769eb2d78501
johannes hanika <hanatos@gmail.com>
2023-10-09 10:30:16 +0200

    rcd: move stuff in place

--- src/pipe/modules/demosaic/wip/config.h
+++
@@ -1,8 +0,0 @@
-// maxComputeWorkGroupInvocations = 1024 on 1650 max-q which means
-// a max of 32x32 local size means max 64x32 tiles
-// maybe we'll have more on better gpu?
-#define DT_RCD_LOCAL_SIZE_X 32
-#define DT_RCD_LOCAL_SIZE_Y 32
-#define DT_RCD_TILE_SIZE_X 64
-#define DT_RCD_TILE_SIZE_Y 32
-#define DT_RCD_BORDER 3
