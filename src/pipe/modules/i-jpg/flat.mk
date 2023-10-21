98571a5de577ae2560a90b246596c11c5705ad3e
Luca Zulberti <zulberti.luca@gmail.com>
2023-10-21 07:53:42 +0200

    Makefiles: Add controls for building on OSX

--- src/pipe/modules/i-jpg/flat.mk
+++ src/pipe/modules/i-jpg/flat.mk
@@ -1,3 +1,3 @@
 MOD_CFLAGS=$(VKDT_JPEG_CFLAGS)
 MOD_LDFLAGS=$(VKDT_JPEG_LDFLAGS)
-pipe/modules/i-jpg/libi-jpg.so:pipe/modules/i-jpg/jpegexiforient.h
+pipe/modules/i-jpg/libi-jpg.$(SEXT):pipe/modules/i-jpg/jpegexiforient.h
