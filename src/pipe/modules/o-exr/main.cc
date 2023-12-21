92d1596390ee60d7ef9db94e3b302e5d92c55d26
johannes hanika <hanatos@gmail.com>
2023-12-21 11:54:03 +0100

    o-exr: write chromaticities
    seems this is only about working if i reverse the BGR order again :(

--- src/pipe/modules/o-exr/main.cc
+++ src/pipe/modules/o-exr/main.cc
@@ -46,9 +46,9 @@ void write_sink(
   }
 
   uint16_t* image_ptr[3];
-  image_ptr[0] = &(images[0].at(0));
+  image_ptr[0] = &(images[2].at(0));
   image_ptr[1] = &(images[1].at(0));
-  image_ptr[2] = &(images[2].at(0));
+  image_ptr[2] = &(images[0].at(0));
 
   img.images = (unsigned char**)image_ptr;
   img.width  = wd;
@@ -56,9 +56,9 @@ void write_sink(
 
   hdr.num_channels = 3;
   hdr.channels = (EXRChannelInfo *)malloc(sizeof(EXRChannelInfo) * hdr.num_channels);
-  strncpy(hdr.channels[0].name, "R", 255); hdr.channels[0].name[strlen("R")] = '\0';
+  strncpy(hdr.channels[0].name, "B", 255); hdr.channels[0].name[strlen("B")] = '\0';
   strncpy(hdr.channels[1].name, "G", 255); hdr.channels[1].name[strlen("G")] = '\0';
-  strncpy(hdr.channels[2].name, "B", 255); hdr.channels[2].name[strlen("B")] = '\0';
+  strncpy(hdr.channels[2].name, "R", 255); hdr.channels[2].name[strlen("R")] = '\0';
 
   hdr.pixel_types = (int *)malloc(sizeof(int) * hdr.num_channels);
   hdr.requested_pixel_types = (int *)malloc(sizeof(int) * hdr.num_channels);
@@ -67,6 +67,17 @@ void write_sink(
     hdr.pixel_types[i] = TINYEXR_PIXELTYPE_HALF;
     hdr.requested_pixel_types[i] = TINYEXR_PIXELTYPE_HALF;
   }
+
+  // rr gg bb ww assuming rec2020 D65
+  float chromaticities[] = { 0.708, 0.292, 0.170, 0.797, 0.131, 0.046, 0.3127, 0.3290};
+  EXRAttribute custom_attributes[] = {{
+    "chromaticities",
+    "Chromaticities",
+    (unsigned char*)chromaticities,
+    sizeof(chromaticities),
+  }};
+  hdr.custom_attributes = custom_attributes;
+  hdr.num_custom_attributes = sizeof(custom_attributes)/sizeof(custom_attributes[0]);
 
   const char* err;
   int ret = SaveEXRImageToFile(&img, &hdr, filename, &err);
