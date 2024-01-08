1fae66fb9acf7d2dce9db7e219d4f9f091680a09
johannes hanika <hanatos@gmail.com>
2024-01-07 16:37:48 -0800

    win: restore lost file

---
+++ src/pipe/asciiio.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string.h>
+#include <stdlib.h>
+
+static inline dt_token_t
+dt_read_token(
+    char  *line,
+    char **out)
+{
+  dt_token_t t = dt_token(line);
+  char *cc = dt_token_str(t), *c = cc;
+  while(c < cc+8 && *c != ':' && *c != '\n' && *c != 0) c++;
+  *out = line + (c-cc);
+  if((*out)[0] == ':' || (*out)[0] == '\n') (*out)++; // don't move past 0 byte
+  for(;c < cc+8;c++) *c = 0;
+  return t;
+}
+
+static inline int
+dt_read_int(char *line, char **out)
+{
+  const int res = strtol(line, out, 10);
+  if(*out != line) *out = *out + 1; // eat : or \n
+  return res;
+}
+
+static inline float
+dt_read_float(char *line, char **out)
+{
+  const float res = strtof(line, out);
+  if(*out != line) *out = *out + 1; // eat : or \n
+  return res;
+}
