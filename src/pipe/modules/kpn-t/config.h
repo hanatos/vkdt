33d411ae245bc9f0dc326ca974668763d06be0aa
johannes hanika <hanatos@gmail.com>
2024-01-16 17:04:43 +0100

    kpn: DEBUG: no convolution is same brightness diff

--- src/pipe/modules/kpn-t/config.h
+++ src/pipe/modules/kpn-t/config.h
@@ -9,8 +9,9 @@
 #define APPLY_SOFTMAX 1
 // #define APPLY_SIGMOID 2
 #define APPLY_PLAIN 3
+#define APPLY_DEBUG 4
 // #define APPLY_ACTIVATION APPLY_SOFTMAX
-#define APPLY_ACTIVATION APPLY_PLAIN // XXX DEBUG
+#define APPLY_ACTIVATION APPLY_DEBUG // XXX DEBUG
 // modes for alpha blending
 #define ALPHA_CONST 1
 #define ALPHA_PLAIN 2
@@ -26,7 +27,7 @@
 // #define MLP_ACTIVATION MLP_ACTIVATION_LEAKY_RELU // best candidate for results
 #define MLP_ACTIVATION MLP_ACTIVATION_NONE // XXX debug deriv outside mlp with large offset DERIV_EPS
 
-// #define DEBUG_DERIV // debug derivatives instead of training
+#define DEBUG_DERIV // debug derivatives instead of training
 #define DERIV_EPS 1 // 1e-1 // lower will only show numeric jitter
 
 #if 0 // check memory bounds before access
