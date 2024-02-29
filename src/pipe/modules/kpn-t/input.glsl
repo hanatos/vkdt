3716ca774fcd882ca450c110b4695d47fc87c13b
johannes hanika <hanatos@gmail.com>
2024-02-29 10:38:33 +0100

    bayer: fix boundary handling

--- src/pipe/modules/kpn-t/input.glsl
+++ src/pipe/modules/kpn-t/input.glsl
@@ -12,10 +12,10 @@ load_input_tap(
   uint j = chan / 8;
   uint i = chan - 8*j;
   px += ivec2(i-3, j-3);
-  if(px.x < 0) px.x = -2*(px.x/2) + (px.x & 1);
-  if(px.x >= textureSize(img, 0).x) px.x = 2*((2*textureSize(img, 0).x - px.x - 1)/2) + (px.x & 1);
-  if(px.y < 0) px.y = -2*(px.y/2) + (px.y & 1);
-  if(px.y >= textureSize(img, 0).y) px.y = 2*((2*textureSize(img, 0).y - px.y - 1)/2) + (px.y & 1);
+  if(px.x < 0) px.x = -2*((px.x+1)/2) + (px.x & 1);
+  if(px.x >= textureSize(img, 0).x) px.x = 2*(textureSize(img, 0).x - px.x/2 - 1) + (px.x & 1);
+  if(px.y < 0) px.y = -2*((px.y+1)/2) + (px.y & 1);
+  if(px.y >= textureSize(img, 0).y) px.y = 2*(textureSize(img, 0).y - px.y/2 - 1) + (px.y & 1);
   return texelFetch(img, px, 0).r;
 #else
 #error demosaicing needs 64 wide networks!
