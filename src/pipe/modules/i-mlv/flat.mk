9c0e1c100afa98ac333e25ef3698d5d511356699
johannes hanika <hanatos@gmail.com>
2024-06-14 14:10:21 +0200

    build: omp and mlv

--- src/pipe/modules/i-mlv/flat.mk
+++ src/pipe/modules/i-mlv/flat.mk
@@ -1,2 +1,8 @@
 MOD_CFLAGS=-fopenmp
 pipe/modules/i-mlv/libi-mlv.so: pipe/modules/i-mlv/mlv.h pipe/modules/i-mlv/raw.h pipe/modules/i-mlv/video_mlv.c pipe/modules/i-mlv/video_mlv.h pipe/modules/i-mlv/liblj92/lj92.c
+ifeq ($(CXX),clang++)
+MOD_LDFLAGS+=-fopenmp=libomp
+endif
+ifeq ($(CXX),g++)
+MOD_LDFLAGS+=-lgomp
+endif
