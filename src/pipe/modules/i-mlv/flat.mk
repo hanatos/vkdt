fc3938492a5313dc6b05bee08e90fa0849048d8e
johannes hanika <hanatos@gmail.com>
2024-06-14 16:14:29 +0200

    build: nightly

--- src/pipe/modules/i-mlv/flat.mk
+++ src/pipe/modules/i-mlv/flat.mk
@@ -1,5 +1,5 @@
 pipe/modules/i-mlv/libi-mlv.so: pipe/modules/i-mlv/mlv.h pipe/modules/i-mlv/raw.h pipe/modules/i-mlv/video_mlv.c pipe/modules/i-mlv/video_mlv.h pipe/modules/i-mlv/liblj92/lj92.c
-ifeq ($(CXX),clang++)
+ifeq ($(CXX),clang)
 MOD_LDFLAGS+=-fopenmp=libomp
 endif
 ifeq ($(CXX),g++)
