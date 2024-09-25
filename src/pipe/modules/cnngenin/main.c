b8c02f97df84fa1a8b64fd9025549d6f2638245c
johannes hanika <hanatos@gmail.com>
2024-09-25 12:44:53 +0200

    cnngenin: add option to output full res degraded image

--- src/pipe/modules/cnngenin/main.c
+++ src/pipe/modules/cnngenin/main.c
@@ -16,13 +16,34 @@ void modify_roi_out(
     dt_graph_t  *graph,
     dt_module_t *module)
 {
-  const int wd = 64;//256;
-  module->connector[2].roi.full_wd = wd;
-  module->connector[2].roi.full_ht = wd;
-  module->connector[3].roi.full_wd = wd;
-  module->connector[3].roi.full_ht = wd;
+  const int tiles = dt_module_param_int(module, dt_module_get_param(module->so, dt_token("tiles")))[0];
+  if(tiles)
+  {
+    const int wd = 64;//256;
+    module->connector[2].roi.full_wd = wd;
+    module->connector[2].roi.full_ht = wd;
+    module->connector[3].roi.full_wd = wd;
+    module->connector[3].roi.full_ht = wd;
+  }
+  else
+  {
+    module->connector[2].roi = module->connector[0].roi;
+    module->connector[3].roi = module->connector[1].roi;
+  }
   module->connector[4].roi.full_wd = 2;
   module->connector[4].roi.full_ht = 2;
+}
+
+dt_graph_run_t
+check_params(
+    dt_module_t *module,
+    uint32_t     parid,
+    uint32_t     num,
+    void        *oldval)
+{
+  if(parid == 1) // tiles
+    return s_graph_run_all; // need no do modify_roi_out again to read noise model from file
+  return s_graph_run_record_cmd_buf;
 }
 
 void create_nodes(
