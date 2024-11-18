333e0dfc5ab5b93b8bb05989803872617ff8a3e2
johannes hanika <hanatos@gmail.com>
2024-11-18 18:30:13 +0100

    gui: property focus group macros

--- src/gui/widget_export.h
+++ src/gui/widget_export.h
@@ -165,7 +165,9 @@ dt_export(
   int resi;
   float resf;
 
+#if 0
   // TODO: wrap into FOCUS_LIST_FIRST and FOCUS_LIST_LAST and pre/post in between macros
+  // focus_group_first
   static int focus_id_next = -1; // target to jump to with focus
   int focus_id_curr = 0; // counter which focus-receiver is next
   static double time_tab;
@@ -176,20 +178,62 @@ dt_export(
     time_tab = time_now;
     tab_keypress = 1;
   }
+  // property here
+  // end
+#endif
 
   resi = w->wd;
+#define focus_group_property_first(TYPE, CTX, NAME, m, V, M, I1, I2)\
+  static int focus_id_next = -1;\
+  int focus_id_curr = 0; \
+  static double time_tab; \
+  double time_now = glfwGetTime(); \
+  int tab_keypress = 0; \
+  if(glfwGetKey(vkdt.win.window, GLFW_KEY_TAB) == GLFW_PRESS && (time_now - time_tab > 0.2)) \
+  { \
+    time_tab = time_now; \
+    tab_keypress = 1; \
+  } \
+  do {\
+    if(focus_id_curr++ == focus_id_next) { nk_property_focus(CTX); focus_id_next = -1; }\
+    nk_property_ ## TYPE(CTX, NAME, m, V, M, I1, I2);\
+    int adv = nk_property_## TYPE ##_unfocus(CTX, NAME, m, V, M, I1, tab_keypress);\
+    if(adv) { tab_keypress = adv = 0; focus_id_next = focus_id_curr; }\
+  } while(0)
+#define focus_group_property(TYPE, CTX, NAME, m, V, M, I1, I2)\
+  do {\
+    if(focus_id_curr++ == focus_id_next) { nk_property_focus(CTX); focus_id_next = -1; }\
+    nk_property_ ## TYPE(CTX, NAME, m, V, M, I1, I2);\
+    int adv = nk_property_## TYPE ##_unfocus(CTX, NAME, m, V, M, I1, tab_keypress);\
+    if(adv) { tab_keypress = adv = 0; focus_id_next = focus_id_curr; }\
+  } while(0)
+#define focus_group_property_last(TYPE, CTX, NAME, m, V, M, I1, I2)\
+  do {\
+    if(focus_id_curr++ == focus_id_next) { nk_property_focus(CTX); focus_id_next = -1; }\
+    nk_property_ ## TYPE(CTX, NAME, m, V, M, I1, I2);\
+    int adv = nk_property_## TYPE ##_unfocus(CTX, NAME, m, V, M, I1, tab_keypress);\
+    if(adv) { tab_keypress = adv = 0; focus_id_next = 0; }\
+  } while(0)
+
+#if 0
   if(focus_id_curr++ == focus_id_next) { nk_property_focus(ctx); focus_id_next = -1; }
   nk_property_int(ctx, "#", 0, &resi, 65535, 1, 1);
   int adv = nk_property_int_unfocus(ctx, "#", 0, &resi, 65535, 1, tab_keypress);
   if(adv) { tab_keypress = adv = 0; focus_id_next = focus_id_curr; }
+#endif
+  focus_group_property_first(int, ctx, "#", 0, &resi, 65535, 1, 1);
+
   if(resi != w->wd) dt_rc_set_int(&vkdt.rc, "gui/export/wd", (w->wd = resi));
   nk_label(ctx, "width", NK_TEXT_LEFT);
 
   resi = w->ht;
+#if 0
   if(focus_id_curr++ == focus_id_next) { nk_property_focus(ctx); focus_id_next = -1; }
   nk_property_int(ctx, "#", 0, &resi, 65535, 1, 1);
   adv = nk_property_int_unfocus(ctx, "#", 0, &resi, 65535, 1, tab_keypress);
   if(adv) { tab_keypress = adv = 0; focus_id_next = 0; } // circle back to top
+#endif
+  focus_group_property_last(int, ctx, "#", 0, &resi, 65535, 1, 1);
 
   if(resi != w->ht) dt_rc_set_int(&vkdt.rc, "gui/export/ht", (w->ht = resi));
   nk_label(ctx, "height", NK_TEXT_LEFT);
