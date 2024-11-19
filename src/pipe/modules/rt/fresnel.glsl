a7ac56dc39ede34454cba42f8cc58c6cf107f90c
johannes hanika <hanatos@gmail.com>
2024-11-19 17:13:59 +0100

    fresnel: add copper fits

--- src/pipe/modules/rt/fresnel.glsl
+++ src/pipe/modules/rt/fresnel.glsl
@@ -9,6 +9,11 @@ fresnel(
   // turingbot says this is a good fit in 380--720 (diverges a bit away around 800 nm)
   vec4 ior_Au = 1.16308-sin(1.14531*(0.457845+cos(0.00894816*(20.1947*cos(0.0334696*(24.8434+lambda))+lambda))));
   vec4 kappa_Au = 0.00738787*((2.33998-cos(0.0344368*(lambda-20.7609)))*(30.9339*cos(5.48433*log(lambda))-10.4363-1.58234*cos(lambda*(-7.3151e-05)*lambda))+lambda-124.02);
+#if 0
+  // is a bit off below 400nm
+  vec4 ior_Cu = 1.21333+cos(0.00395433*(lambda+cos(0.0394955*lambda)/0.052127+log((-356.328)+lambda)*cos(0.0188123*lambda)/0.0518843));
+  vec4 kappa_Cu = 0.00693785*(56.1866*cos((-0.0164339)*(lambda-24.9456*cos(max(-18.8496,(-0.0244734)*lambda))))+lambda-33.6468-97.3276);
+#endif
 
   vec4 ret;
   vec2 n2, ratio, cost;
