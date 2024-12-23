24dbd8cc9ae025aaf3ffc25e264f63b3e7485971
johannes hanika <hanatos@gmail.com>
2024-12-23 12:34:48 +0100

    nuklear: increase max float precision to at least more than 8-bit

--- src/gui/nuklear.h
+++ src/gui/nuklear.h
@@ -5813,7 +5813,7 @@ struct nk_context {
 #define NK_PI 3.141592654f
 #define NK_PI_HALF 1.570796326f
 #define NK_UTF_INVALID 0xFFFD
-#define NK_MAX_FLOAT_PRECISION 2
+#define NK_MAX_FLOAT_PRECISION 4
 
 #define NK_UNUSED(x) ((void)(x))
 #define NK_SATURATE(x) (NK_MAX(0, NK_MIN(1.0f, x)))
