d9e95ac97dfbd975e4682837fc6d1e5418e7e790
johannes hanika <hanatos@gmail.com>
2025-03-29 21:19:22 +0100

    curves: cubic spline

--- src/pipe/modules/curves/main.c
+++ src/pipe/modules/curves/main.c
@@ -1,12 +1,65 @@
 #include "modules/api.h"
 
 void modify_roi_out(
-    dt_graph_t *graph,
+    dt_graph_t  *graph,
     dt_module_t *module)
 { // request something square for dspy output
   module->connector[2].roi.full_wd = 1024;
   module->connector[2].roi.full_ht = 1024;
   module->connector[1].roi = module->connector[0].roi; // output
+}
+
+void commit_params(dt_graph_t *graph, dt_module_t *mod)
+{
+  int *p_cnt = (int   *)dt_module_param_int(mod, dt_module_get_param(mod->so, dt_token("cnt")));
+  float *p_x = (float *)dt_module_param_int(mod, dt_module_get_param(mod->so, dt_token("x")));
+  float *p_y = (float *)dt_module_param_int(mod, dt_module_get_param(mod->so, dt_token("y")));
+
+  float ddx0 = 0.0f, ddxn = 0.0f; // boundary constraints, set them according to some parameter
+
+  // cnt is 2..8
+  float alpha[8], h[8], I[8], mu[8], z[8];
+  float a[8], b[8], c[8], d[8];
+
+  // clamped cubic spline
+  int n = p_cnt[0]-1;
+  for(int i=0;i<n;i++)
+  {
+    a[i] = p_y[i];
+    h[i] = p_x[i+1] - p_x[i];
+  }
+  a[n] = p_y[n];
+  alpha[0] = 3.0f*(a[1]-a[0])/h[0] - 3.0f*ddx0;
+  alpha[n] = 3.0f*ddxn - 3.0f*(a[n]-a[n-1])/h[n-1];
+  for(int i=1;i<n;i++)
+    alpha[i] = 3.0f/h[i]*(a[i+1]-a[i]) - 3.0f/h[i-1]*(a[i]-a[i-1]);
+  I[0]  = 2.0f*h[0];
+  mu[0] = 0.5f;
+  z[0] = alpha[0]/I[0];
+
+  for(int i=1;i<n;i++)
+  {
+    I[i] = 2.0f*(p_x[i+1] - p_x[i-1]) - h[i-1]*mu[i-1];
+    mu[i] = h[i]/I[i];
+    z[i] = (alpha[i] - h[i-1]*z[i-1])/I[i];
+  }
+  I[n] = h[n-1]*(2.0f-mu[n-1]);
+  z[n] = (alpha[n] - h[n-1]*z[n-1])/I[n];
+  c[n] = z[n];
+  for(int j=n-1;j>=0;j--)
+  {
+    c[j] = z[j] - mu[j]*c[j+1];
+    b[j] = (a[j+1] - a[j])/h[j] - h[j]*(c[j+1]+2.0f*c[j])/3.0f;
+    d[j] = (c[j+1]-c[j])/(3.0f*h[j]);
+  }
+  float *p_abcd = (float *)dt_module_param_int(mod, dt_module_get_param(mod->so, dt_token("abcd")));
+  for(int i=0;i<n;i++)
+  {
+    p_abcd[4*i+0] = a[i];
+    p_abcd[4*i+1] = b[i];
+    p_abcd[4*i+2] = c[i];
+    p_abcd[4*i+3] = d[i];
+  }
 }
 
 dt_graph_run_t
