a9dca9d6b7e911a9bf18411820d0883da0d23b6e
johannes hanika <hanatos@gmail.com>
2025-03-30 10:54:54 +0200

    curves: rgb curves

--- src/pipe/modules/curves/main.c
+++ src/pipe/modules/curves/main.c
@@ -11,54 +11,62 @@ void modify_roi_out(
 
 void commit_params(dt_graph_t *graph, dt_module_t *mod)
 {
-  int *p_cnt = (int   *)dt_module_param_int(mod, dt_module_get_param(mod->so, dt_token("cnt")));
-  float *p_x = (float *)dt_module_param_int(mod, dt_module_get_param(mod->so, dt_token("x")));
-  float *p_y = (float *)dt_module_param_int(mod, dt_module_get_param(mod->so, dt_token("y")));
-
-  float ddx0 = 0.0f, ddxn = 0.0f; // boundary constraints, set them according to some parameter
-
-  // cnt is 2..8
-  float alpha[8], h[8], I[8], mu[8], z[8];
-  float a[8], b[8], c[8], d[8];
-
-  // clamped cubic spline
-  int n = p_cnt[0]-1;
-  for(int i=0;i<n;i++)
+  dt_token_t par[] = {
+    dt_token("cntr"), dt_token("xr"), dt_token("yr"), dt_token("abcdr"), dt_token("ddr0"), dt_token("ddrn"),
+    dt_token("cntg"), dt_token("xg"), dt_token("yg"), dt_token("abcdg"), dt_token("ddg0"), dt_token("ddgn"),
+    dt_token("cntb"), dt_token("xb"), dt_token("yb"), dt_token("abcdb"), dt_token("ddb0"), dt_token("ddbn")};
+  for(int channel=0;channel<3;channel++)
   {
-    a[i] = p_y[i];
-    h[i] = p_x[i+1] - p_x[i];
-  }
-  a[n] = p_y[n];
-  alpha[0] = 3.0f*(a[1]-a[0])/h[0] - 3.0f*ddx0;
-  alpha[n] = 3.0f*ddxn - 3.0f*(a[n]-a[n-1])/h[n-1];
-  for(int i=1;i<n;i++)
-    alpha[i] = 3.0f/h[i]*(a[i+1]-a[i]) - 3.0f/h[i-1]*(a[i]-a[i-1]);
-  I[0]  = 2.0f*h[0];
-  mu[0] = 0.5f;
-  z[0] = alpha[0]/I[0];
+    int *p_cnt = (int   *)dt_module_param_int  (mod, dt_module_get_param(mod->so, par[6*channel+0]));
+    float *p_x = (float *)dt_module_param_float(mod, dt_module_get_param(mod->so, par[6*channel+1]));
+    float *p_y = (float *)dt_module_param_float(mod, dt_module_get_param(mod->so, par[6*channel+2]));
 
-  for(int i=1;i<n;i++)
-  {
-    I[i] = 2.0f*(p_x[i+1] - p_x[i-1]) - h[i-1]*mu[i-1];
-    mu[i] = h[i]/I[i];
-    z[i] = (alpha[i] - h[i-1]*z[i-1])/I[i];
-  }
-  I[n] = h[n-1]*(2.0f-mu[n-1]);
-  z[n] = (alpha[n] - h[n-1]*z[n-1])/I[n];
-  c[n] = z[n];
-  for(int j=n-1;j>=0;j--)
-  {
-    c[j] = z[j] - mu[j]*c[j+1];
-    b[j] = (a[j+1] - a[j])/h[j] - h[j]*(c[j+1]+2.0f*c[j])/3.0f;
-    d[j] = (c[j+1]-c[j])/(3.0f*h[j]);
-  }
-  float *p_abcd = (float *)dt_module_param_int(mod, dt_module_get_param(mod->so, dt_token("abcd")));
-  for(int i=0;i<n;i++)
-  {
-    p_abcd[4*i+0] = a[i];
-    p_abcd[4*i+1] = b[i];
-    p_abcd[4*i+2] = c[i];
-    p_abcd[4*i+3] = d[i];
+    float ddx0 = dt_module_param_float(mod, dt_module_get_param(mod->so, par[6*channel+4]))[0];
+    float ddxn = dt_module_param_float(mod, dt_module_get_param(mod->so, par[6*channel+5]))[0];
+
+    // cnt is 2..8
+    float alpha[8], h[8], I[8], mu[8], z[8];
+    float a[8], b[8], c[8], d[8];
+
+    // clamped cubic spline
+    int n = p_cnt[0]-1;
+    for(int i=0;i<n;i++)
+    {
+      a[i] = p_y[i];
+      h[i] = p_x[i+1] - p_x[i];
+    }
+    a[n] = p_y[n];
+    alpha[0] = 3.0f*(a[1]-a[0])/h[0] - 3.0f*ddx0;
+    alpha[n] = 3.0f*ddxn - 3.0f*(a[n]-a[n-1])/h[n-1];
+    for(int i=1;i<n;i++)
+      alpha[i] = 3.0f/h[i]*(a[i+1]-a[i]) - 3.0f/h[i-1]*(a[i]-a[i-1]);
+    I[0]  = 2.0f*h[0];
+    mu[0] = 0.5f;
+    z[0] = alpha[0]/I[0];
+
+    for(int i=1;i<n;i++)
+    {
+      I[i] = 2.0f*(p_x[i+1] - p_x[i-1]) - h[i-1]*mu[i-1];
+      mu[i] = h[i]/I[i];
+      z[i] = (alpha[i] - h[i-1]*z[i-1])/I[i];
+    }
+    I[n] = h[n-1]*(2.0f-mu[n-1]);
+    z[n] = (alpha[n] - h[n-1]*z[n-1])/I[n];
+    c[n] = z[n];
+    for(int j=n-1;j>=0;j--)
+    {
+      c[j] = z[j] - mu[j]*c[j+1];
+      b[j] = (a[j+1] - a[j])/h[j] - h[j]*(c[j+1]+2.0f*c[j])/3.0f;
+      d[j] = (c[j+1]-c[j])/(3.0f*h[j]);
+    }
+    float *p_abcd = (float *)dt_module_param_float(mod, dt_module_get_param(mod->so, par[6*channel+3]));
+    for(int i=0;i<n;i++)
+    {
+      p_abcd[4*i+0] = a[i];
+      p_abcd[4*i+1] = b[i];
+      p_abcd[4*i+2] = c[i];
+      p_abcd[4*i+3] = d[i];
+    }
   }
 }
 
@@ -67,10 +75,15 @@ input(
     dt_module_t             *mod,
     dt_module_input_event_t *p)
 { // mouse events on our dspy curve output
-  int *p_cnt = (int   *)dt_module_param_int(mod, dt_module_get_param(mod->so, dt_token("cnt")));
-  int *p_sel = (int   *)dt_module_param_int(mod, dt_module_get_param(mod->so, dt_token("sel")));
-  float *p_x = (float *)dt_module_param_int(mod, dt_module_get_param(mod->so, dt_token("x")));
-  float *p_y = (float *)dt_module_param_int(mod, dt_module_get_param(mod->so, dt_token("y")));
+  dt_token_t par[] = {
+    dt_token("cntr"), dt_token("xr"), dt_token("yr"),
+    dt_token("cntg"), dt_token("xg"), dt_token("yg"),
+    dt_token("cntb"), dt_token("xb"), dt_token("yb")};
+  int channel = dt_module_param_int(mod, dt_module_get_param(mod->so, dt_token("channel")))[0];
+  int *p_sel = (int   *)dt_module_param_int  (mod, dt_module_get_param(mod->so, dt_token("sel")));
+  int *p_cnt = (int   *)dt_module_param_int  (mod, dt_module_get_param(mod->so, par[3*channel+0]));
+  float *p_x = (float *)dt_module_param_float(mod, dt_module_get_param(mod->so, par[3*channel+1]));
+  float *p_y = (float *)dt_module_param_float(mod, dt_module_get_param(mod->so, par[3*channel+2]));
 
   static int active = -1;
   if(p->type == 1)
