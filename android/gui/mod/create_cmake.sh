a7edf45b13132c32d506700cfc121f1b7f2cba36
johannes hanika <hanatos@gmail.com>
2025-08-11 10:02:54 +0200

    android: cmake generator script

---
+++ android/gui/mod/create_cmake.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# re-create sub directories and their CMakeLists.txt files
+# according to the modules in the real source tree.
+
+# main.c
+cd ../../../src/pipe/modules
+mods=$(find . -name main.c | cut -f2 -d/ | uniq)
+cd -
+for m in $mods
+do
+  mkdir -p $m
+  echo << EOF > $m/CMakeLists.txt
+set(NAME $m)
+set(MOD $m)
+set(SRC_DIR ../../../../src)
+set(MOD_DIR ${SRC_DIR}/pipe/modules/${MOD})
+set(ASS_DIR ../../assets)
+include_directories(${SRC_DIR})
+include_directories(${MOD_DIR})
+
+add_library($m SHARED ${MOD_DIR}/main.c)
+# target_link_libraries($m z)
+EOF
+done
+
+
+# main.cc
