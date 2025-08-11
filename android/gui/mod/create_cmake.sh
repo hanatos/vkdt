cf9295c064b4d38ca0b27f00e9d95a5164184acd
johannes hanika <hanatos@gmail.com>
2025-08-11 11:02:26 +0200

    android: build modules boilerplate

--- android/gui/mod/create_cmake.sh
+++ android/gui/mod/create_cmake.sh
@@ -2,6 +2,7 @@
 # re-create sub directories and their CMakeLists.txt files
 # according to the modules in the real source tree.
 
+>../mods.cmake
 # main.c
 cd ../../../src/pipe/modules
 mods=$(find . -name main.c | cut -f2 -d/ | uniq)
@@ -9,16 +10,17 @@ cd -
 for m in $mods
 do
   mkdir -p $m
-  echo << EOF > $m/CMakeLists.txt
+  echo "add_subdirectory(mod/$m)" >> ../mods.cmake
+  cat << EOF > $m/CMakeLists.txt
 set(NAME $m)
 set(MOD $m)
 set(SRC_DIR ../../../../src)
-set(MOD_DIR ${SRC_DIR}/pipe/modules/${MOD})
+set(MOD_DIR \${SRC_DIR}/pipe/modules/\${MOD})
 set(ASS_DIR ../../assets)
-include_directories(${SRC_DIR})
-include_directories(${MOD_DIR})
+include_directories(\${SRC_DIR})
+include_directories(\${MOD_DIR})
 
-add_library($m SHARED ${MOD_DIR}/main.c)
+add_library($m SHARED \${MOD_DIR}/main.c)
 # target_link_libraries($m z)
 EOF
 done
