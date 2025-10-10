48ed04522cedeb0e8ca41d872b0d0efc9b1b652e
johannes hanika <hanatos@gmail.com>
2025-10-10 15:42:10 +0200

    android: compile fix

--- src/pipe/global.c
+++ src/pipe/global.c
@@ -412,7 +412,7 @@ compare_module_name(const void *a, const void *b)
   return strncmp(dt_token_str(ma->name), dt_token_str(mb->name), 8);
 }
 
-int dt_pipe_global_init()
+int dt_pipe_global_init(void *appv)
 {
   memset(&dt_pipe, 0, sizeof(dt_pipe));
 #ifdef __ANDROID__
