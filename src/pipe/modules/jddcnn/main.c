a02ce8758dbf27eabf04497a27fb3716918c8ec6
johannes hanika <hanatos@gmail.com>
2025-11-17 09:04:42 +0100

    jddcnn: make sure to work full res in/out

--- src/pipe/modules/jddcnn/main.c
+++ src/pipe/modules/jddcnn/main.c
@@ -16,9 +16,10 @@ void modify_roi_in(
     dt_graph_t  *graph,
     dt_module_t *module)
 {
+  dt_roi_t *ro = &module->connector[1].roi;
   dt_roi_t *ri = &module->connector[0].roi;
-  ri->wd = ri->full_wd;
-  ri->ht = ri->full_ht;
+  ro->wd = ri->wd = ri->full_wd;
+  ro->ht = ri->ht = ri->full_ht;
   ri->scale = 1.0f;
 }
 
