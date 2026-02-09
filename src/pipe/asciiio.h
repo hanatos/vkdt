6e6595f501caac8c1b8607c4a1fe68b294f133ed
johannes hanika <hanatos@gmail.com>
2026-02-09 11:43:01 +0100

    io: valgrind happiness

--- src/pipe/asciiio.h
+++ src/pipe/asciiio.h
@@ -20,15 +20,17 @@ dt_read_token(
 static inline int
 dt_read_int(char *line, char **out)
 {
+  if(line[0] == 0) return 0;
   const int res = strtol(line, out, 10);
-  if(*out != line) *out = *out + 1; // eat : or \n
+  if(**out && *out != line) *out = *out + 1; // eat : or \n
   return res;
 }
 
 static inline float
 dt_read_float(char *line, char **out)
 {
+  if(line[0] == 0) return 0.0f;
   const float res = strtof(line, out);
-  if(*out != line) *out = *out + 1; // eat : or \n
+  if(**out && *out != line) *out = *out + 1; // eat : or \n
   return res;
 }
