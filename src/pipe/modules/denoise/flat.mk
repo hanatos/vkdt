ba28ebe9249121f2f25a902a066bbf2b8a728fbf
johannes hanika <hanatos@gmail.com>
2026-04-22 12:53:40 +0200

    denoise: use dng spec noise profile values

--- src/pipe/modules/denoise/flat.mk
+++ src/pipe/modules/denoise/flat.mk
@@ -1,3 +1,3 @@
 MOD_LDFLAGS=-lm
-pipe/modules/denoise/down.comp.spv   :pipe/modules/denoise/cov.glsl
-pipe/modules/denoise/downcov.comp.spv:pipe/modules/denoise/cov.glsl
+pipe/modules/denoise/down.comp.spv   :pipe/modules/denoise/cov.glsl pipe/modules/denoise/noise.glsl
+pipe/modules/denoise/downcov.comp.spv:pipe/modules/denoise/cov.glsl pipe/modules/denoise/noise.glsl
--- src/pipe/modules/denoise/noise.glsl
+++ src/pipe/modules/denoise/noise.glsl
@@ -1,7 +1,9 @@
 float noise_sigma(
     float noise_a, // gaussian part of the noise
     float noise_b, // poissonian part of the noise
-    float val)     // pixel value, in the raw 16-bit range of 0..65535
+    float black,
+    float white,
+    float val)     // pixel value in [0, 1]
 {
-  return 5.0 * sqrt(noise_a + val*noise_b);
+  return 2.0*sqrt(noise_a + max(0, (val-black)/(white-black))*noise_b);
 }
