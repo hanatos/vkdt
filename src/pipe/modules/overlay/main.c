299caddd696ddbf710e46be401454922774195c5
johannes hanika <hanatos@gmail.com>
2026-05-19 18:16:23 +0200

    overlay: left and right text

--- src/pipe/modules/overlay/main.c
+++ src/pipe/modules/overlay/main.c
@@ -57,6 +57,28 @@ nk_utf_decode(const char *c, uint32_t *u, int clen)
   nk_utf_validate(u, len);
   return len;
 }
+int
+nk_utf_len(const char *str, int len)
+{
+    const char *text;
+    int glyphs = 0;
+    int text_len;
+    int glyph_len;
+    int src_len = 0;
+    uint32_t unicode;
+
+    if (!str || !len) return 0;
+
+    text = str;
+    text_len = len;
+    glyph_len = nk_utf_decode(text, &unicode, text_len);
+    while (glyph_len && src_len < len) {
+        glyphs++;
+        src_len = src_len + glyph_len;
+        glyph_len = nk_utf_decode(text + src_len, &unicode, text_len - src_len);
+    }
+    return glyphs;
+}
 #endif
 
 typedef struct overlay_t
@@ -66,11 +88,9 @@ typedef struct overlay_t
   uint8_t        *font_img;
   char            filename[PATH_MAX];
 
-  // TODO how to represent the text? one for each corner?
-  // TODO replace a couple of symbols?
-  char text[256];
-  int text_len;
-  // TODO something about position
+  char textl[256], textr[256];
+  int  textl_len,  textr_len;
+  // TODO position
 } overlay_t;
 
 int init(dt_module_t *mod)
@@ -94,17 +114,36 @@ void cleanup(dt_module_t *mod)
   mod->data = 0;
 }
 
+#if 0
+dt_graph_run_t
+check_params(
+    dt_module_t *module,
+    uint32_t     parid,
+    uint32_t     num,
+    void        *oldval)
+{
+}
+#endif
+
 void commit_params(dt_graph_t *graph, dt_module_t *module)
 {
   overlay_t *ov = module->data;
   // TODO replace text! overwrite text_len with utf-8 length!
+  const int pid_txtl = dt_module_get_param(module->so, dt_token("text l"));
+  const char *p_txtl = dt_module_param_string(module, pid_txtl);
+  const int pid_txtr = dt_module_get_param(module->so, dt_token("text r"));
+  const char *p_txtr = dt_module_param_string(module, pid_txtr);
+  snprintf(ov->textl, sizeof(ov->textl), "%s", p_txtl);
+  snprintf(ov->textr, sizeof(ov->textr), "%s", p_txtr);
+  ov->textl_len = nk_utf_len(ov->textl, strlen(ov->textl));
+  ov->textr_len = nk_utf_len(ov->textr, strlen(ov->textr));
   for(int n=0;n<graph->num_nodes;n++)
   {
     if(graph->node[n].name == dt_token("overlay") &&
        graph->node[n].kernel == dt_token("main") &&
        graph->node[n].module->inst == module->inst)
     {
-      graph->node[n].vtx_cnt = 6*ov->text_len;
+      graph->node[n].vtx_cnt = 6*(ov->textl_len + ov->textr_len);
       break;
     }
   }
@@ -114,10 +153,9 @@ void modify_roi_out(
     dt_graph_t  *graph,
     dt_module_t *module)
 {
+  module->flags |= s_module_request_read_source;
   module->connector[0].roi = (dt_roi_t){ .full_wd = 1024, .full_ht = 1024, .marker = s_roi_mark_dontcare };
   overlay_t *ov = module->data;
-  const int pid_text = dt_module_get_param(module->so, dt_token("text"));
-  const char *p_text = dt_module_param_string(module, pid_text);
   const int pid_font = dt_module_get_param(module->so, dt_token("font"));
   const char *p_font = dt_module_param_string(module, pid_font);
 
@@ -130,8 +168,6 @@ void modify_roi_out(
     memset(ov, 0, sizeof(*ov));
   }
   snprintf(ov->filename, sizeof(ov->filename), "%s", p_font);
-  snprintf(ov->text, sizeof(ov->text), "%s", p_text);
-  ov->text_len = strlen(ov->text); // XXX this should be utf-8 length
 
   char tmp[PATH_MAX] = {0};
   snprintf(tmp, sizeof(tmp), "%s_msdf.lut", p_font);
@@ -213,40 +249,43 @@ int read_source(
     vtx_t *vert = mapped;
     uint8_t col[] = {0xff, 0xff, 0xff, 0xff}; // TODO
     int vidx = 0;
-    uint32_t unicode, next;
-    int text_len = 0, len = strlen(ov->text);
-    len = MIN(len, ov->text_len);
-    len = MIN(len, sizeof(ov->text));
-    float x = 0.f; // XXX positioning! 0,0 is center of image
-    float y = 0.f;
-    float font_height = 0.1f;
-    int glyph_len = nk_utf_decode(ov->text, &unicode, len);
-    while(text_len < len && glyph_len)
+    const float font_height = 0.1f;
+    for(int i=0;i<2;i++)
     {
-      if (unicode == NK_UTF_INVALID) break;
-      int next_glyph_len = nk_utf_decode(
-          ov->text + text_len + glyph_len, &next, len - text_len);
-      dt_font_glyph_t *gg = ov->font.glyph + dt_font_glyph(&ov->font, unicode);
-      float scale = gg->height_to_em * font_height;
+      uint32_t unicode, next;
+      int text_len = 0, len = strlen(i ? ov->textr : ov->textl);
+      len = MIN(len, i ? ov->textr_len : ov->textl_len);
+      len = MIN(len, i ? sizeof(ov->textr) : sizeof(ov->textl));
+      float x = i ? 0.9f-len*font_height*0.333 : -0.9f; // positioning: 0,0 is center of image
+      float y = -0.8f;
+      int glyph_len = nk_utf_decode(i ? ov->textr : ov->textl, &unicode, len);
+      while(text_len < len && glyph_len)
+      {
+        if (unicode == NK_UTF_INVALID) break;
+        int next_glyph_len = nk_utf_decode(
+            (i ? ov->textr : ov->textl) + text_len + glyph_len, &next, len - text_len);
+        dt_font_glyph_t *gg = ov->font.glyph + dt_font_glyph(&ov->font, unicode);
+        float scale = gg->height_to_em * font_height;
 
-      float gx = x + scale * gg->pbox_x;
-      float gy = y + scale + scale * gg->pbox_y;
-      float gw = scale * gg->pbox_w, gh = scale * gg->pbox_h;
-      float char_width = scale * gg->advance;
-      float u0 = gg->tbox_x, v0 = gg->tbox_y, u1 = u0 + gg->tbox_w, v1 = v0 + gg->tbox_h;
-      // push 6 vertices/2 triangles
-      vert[vidx++] = genv(gx,    gy,    u0, v0, col);
-      vert[vidx++] = genv(gx+gw, gy,    u1, v0, col);
-      vert[vidx++] = genv(gx+gw, gy+gh, u1, v1, col);
-      vert[vidx++] = genv(gx,    gy,    u0, v0, col);
-      vert[vidx++] = genv(gx+gw, gy+gh, u1, v1, col);
-      vert[vidx++] = genv(gx   , gy+gh, u0, v1, col);
-      if(vidx >= p->node->connector[0].roi.wd) break;
+        float gx = x + scale * gg->pbox_x;
+        float gy = y + scale + scale * gg->pbox_y;
+        float gw = scale * gg->pbox_w, gh = scale * gg->pbox_h;
+        float char_width = scale * gg->advance;
+        float u0 = gg->tbox_x, v0 = gg->tbox_y, u1 = u0 + gg->tbox_w, v1 = v0 + gg->tbox_h;
+        // push 6 vertices/2 triangles
+        vert[vidx++] = genv(gx,    gy,    u0, v0, col);
+        vert[vidx++] = genv(gx+gw, gy,    u1, v0, col);
+        vert[vidx++] = genv(gx+gw, gy+gh, u1, v1, col);
+        vert[vidx++] = genv(gx,    gy,    u0, v0, col);
+        vert[vidx++] = genv(gx+gw, gy+gh, u1, v1, col);
+        vert[vidx++] = genv(gx   , gy+gh, u0, v1, col);
+        if(vidx >= p->node->connector[0].roi.wd) break;
 
-      text_len += glyph_len;
-      x += char_width;
-      glyph_len = next_glyph_len;
-      unicode = next;
+        text_len += glyph_len;
+        x += char_width;
+        glyph_len = next_glyph_len;
+        unicode = next;
+      }
     }
   }
   return 0;
@@ -260,17 +299,13 @@ create_nodes(
   overlay_t *ov = module->data;
   const int wd = module->connector[0].roi.wd;
   const int ht = module->connector[0].roi.ht;
-  dt_roi_t roi_vtx  = { .wd = sizeof(ov->text), .ht = 6*sizeof(vtx_t) };
-  dt_roi_t roi_font = { .wd = ov->font_hdr.wd,  .ht = ov->font_hdr.ht };
+  dt_roi_t roi_vtx  = { .wd = 2*sizeof(ov->textl), .ht = 6*sizeof(vtx_t) };
+  dt_roi_t roi_font = { .wd = ov->font_hdr.wd,     .ht = ov->font_hdr.ht };
   const int id_font = dt_node_add(graph, module, "overlay", "font", 1, 1, 1, 0, 0, 1,
       "font", "source", "rgba", "ui8", &roi_font);
   const int id_vtx  = dt_node_add(graph, module, "overlay", "vtx", 1, 1, 1, 0, 0, 1,
       "vtx", "source", "ssbo", "ui8", &roi_vtx);
-  // float strength = 0.5f;
-  // uint32_t strengthi = dt_touint(strength);
-  // int pc[] = { strengthi };
   const int id_overlay = dt_node_add(graph, module, "overlay", "main", wd, ht, 1,
-      // sizeof(pc), pc, 3,
       0, 0, 3,
       "vtx",    "read",  "ssbo", "*",    dt_no_roi,
       "font",   "read",  "*",    "*",    dt_no_roi,
