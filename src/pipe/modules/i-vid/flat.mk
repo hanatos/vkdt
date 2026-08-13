4f10c633f6c213d08c2561b9253f25e3d9234f9d
johannes hanika <hanatos@gmail.com>
2026-08-13 15:23:40 +0200

    build: include vulkan in i-vid module for video decode queue stuff

--- src/pipe/modules/i-vid/flat.mk
+++ src/pipe/modules/i-vid/flat.mk
@@ -1,5 +1,5 @@
 ifeq ($(VKDT_USE_FFMPEG),1)
 MOD_CFLAGS=$(VKDT_AV_CFLAGS)
-MOD_LDFLAGS=$(VKDT_AV_LDFLAGS)
+MOD_LDFLAGS=$(VKDT_AV_LDFLAGS) $(VKDT_VULKAN_LDFLAGS)
 endif
 pipe/modules/i-vid/libi-vid.so: pipe/modules/i-vid/vid.h pipe/modules/i-vid/vid-init.c pipe/modules/i-vid/vid-run.c
